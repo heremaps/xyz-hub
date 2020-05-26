d0b9d259014c25c1bdefe70a9421eb3303176934
qGYdXbY2 <47661341+qGYdXbY2@users.noreply.github.com>
2020-05-26 09:58:58 +0200

    extended hexbin recognize lines and polygons (#173)
    * lines & areas v.01
    
    * clip large areas
    
    * fx invalid geom

--- xyz-psql-connector/src/main/java/com/here/xyz/psql/SQLQueryBuilder.java
+++ xyz-psql-connector/src/main/java/com/here/xyz/psql/SQLQueryBuilder.java
@@ -171,8 +171,7 @@ public class SQLQueryBuilder {
             query.addParameter(SQLQuery.createSQLArray(jpath.toArray(new String[]{}), "text", dataSource));
         }
 
-        //query.append(" case st_geometrytype(geo) when 'ST_Point' then geo else st_intersection( geo ," ); query.append( expBboxSql ); query.append(" ) end as geo ");
-        query.append(" case st_geometrytype(geo) when 'ST_Point' then geo else st_force3d(st_closestpoint(geo, geo)) end as refpt ");
+        query.append(String.format( " case st_geometrytype(geo) when 'ST_Point' then geo else st_force3d(st_setsrid( h3ToGeoDeg( coveringDeg( case ST_Within(geo, %2$s ) when true then geo else ST_Intersection( ST_MakeValid(geo), %2$s ) end, %1$d)), st_srid(geo))) end as refpt ",h3res, expBboxSql));
         query.append(" from ${schema}.${table} v where 1 = 1 and geo && ");
         query.append(expBboxSql);
         query.append(" and st_intersects( geo ,");
