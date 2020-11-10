1a0c3f36d65225f413b7c3188dd6edb40f287976
Xue, Haifeng <haifeng.xue@here.com>
2020-11-10 17:34:23 +0100

    Updated, BroadcastLog message should not be a relayed message
    Signed-off-by: Xue, Haifeng <haifeng.xue@here.com>

--- xyz-hub-service/src/main/java/com/here/xyz/hub/rest/admin/messages/BroadcastLog.java
+++ xyz-hub-service/src/main/java/com/here/xyz/hub/rest/admin/messages/BroadcastLog.java
@@ -18,8 +18,7 @@
  */
 
 package com.here.xyz.hub.rest.admin.messages;
-
-import com.here.xyz.hub.rest.admin.messages.RelayedMessage;
+import com.here.xyz.hub.rest.admin.AdminMessage;
 import com.here.xyz.hub.rest.admin.Node;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -27,7 +26,7 @@ import org.apache.logging.log4j.Logger;
 /**
  * A message which can be used to write a log on all nodes at (nearly) the same time.
  */
-public class BroadcastLog extends RelayedMessage {
+public class BroadcastLog extends AdminMessage {
 
   private static final Logger logger = LogManager.getLogger();
 
@@ -44,7 +43,7 @@ public class BroadcastLog extends RelayedMessage {
   }
 
   @Override
-  protected void handleAtDestination() {
+  protected void handle() {
     logger.info("[BROADCAST from " + source.id + " (" + source.ip + ")] " + logMessage);
   }
 }
