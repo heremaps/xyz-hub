21754682000a414e4cafc5f204ccf67ef502f949
Benjamin Rögner <benjamin.roegner@here.com>
2020-11-12 16:15:49 +0100

    Add 10% spare memory to the off queue's heap storage to reduce the risk of evictions during calls to #add() when all queues are full.
    Signed-off-by: Benjamin Rögner <benjamin.roegner@here.com>

--- xyz-hub-service/src/main/java/com/here/xyz/hub/util/LimitedOffHeapQueue.java
+++ xyz-hub-service/src/main/java/com/here/xyz/hub/util/LimitedOffHeapQueue.java
@@ -26,8 +26,8 @@ public class LimitedOffHeapQueue<E extends OffHeapBuffer> extends LimitedQueue<E
 
   private static final long OH_TTL = 32_000; //ms
   private static final ScheduledExecutorService executors = Executors.newScheduledThreadPool(2);
-  private static final OHCache<byte[], byte[]> ohStorage = OHCacheClient.createCache(Service.configuration.GLOBAL_MAX_QUEUE_SIZE,
-      executors);
+  private static final OHCache<byte[], byte[]> ohStorage = OHCacheClient.createCache(
+      (int) (Service.configuration.GLOBAL_MAX_QUEUE_SIZE * 1.1), executors);
 
   public LimitedOffHeapQueue(long maxSize, long maxByteSize) {
     super(maxSize, maxByteSize);
