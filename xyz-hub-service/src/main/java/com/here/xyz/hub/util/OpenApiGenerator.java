0934e7203884456a1186b1e35b7ae958b5a4f1db
Lucas Ceni <lucas.ceni@here.com>
2021-02-12 19:21:58 +0100

    Introduces global string replacements, recipe hierarchy, value replacements
    Signed-off-by: Lucas Ceni <lucas.ceni@here.com>

--- xyz-hub-service/src/main/java/com/here/xyz/hub/util/OpenApiGenerator.java
+++ xyz-hub-service/src/main/java/com/here/xyz/hub/util/OpenApiGenerator.java
@@ -11,12 +11,11 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
+import java.util.function.Consumer;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 
@@ -27,6 +26,7 @@ public class OpenApiGenerator {
   private static final String KEY = "key";
   private static final String VALUE = "value";
   private static final String TYPE = "type";
+  private static final String PATH = "path";
   private static final String FIND = "find";
   private static final String REGEX_SEARCH = "~=";
   private static final String EXACT_SEARCH = "=";
@@ -37,11 +37,12 @@ public class OpenApiGenerator {
   private static final String EXTENDS = "extends";
 
   private static final ObjectMapper YAML_MAPPER = ObjectMapperFactory.createYaml();
-  private static final Set<String> RECIPES_NAMES = new HashSet<>();
+  private static final Map<String, JsonNode> RECIPES_MAP = new HashMap<>();
   private static final Map<String, String> VALUES_MAP = new HashMap<String, String>() {{
     put(VERSION, Service.BUILD_VERSION);
   }};
 
+  private static boolean debug;
   private static JsonNode root;
   private static JsonNode recipe;
 
@@ -58,14 +59,17 @@ public class OpenApiGenerator {
   public static byte[] generate(byte[] sourceBytes, byte[] recipeBytes, final String name) throws Exception {
     recipe = YAML_MAPPER.readTree(recipeBytes);
 
-    validateExtendedRecipe();
+    validateRecipe();
     validateRecipeName(name);
     validateInheritance();
     validateValues();
 
     loadValues();
-    prepareRecipe();
+    prepareRecipe(name);
 
+    if (debug) {
+      FileUtils.writeByteArrayToFile(new File("processedRecipe.yaml"), YAML_MAPPER.writeValueAsString(recipe).getBytes());
+    }
     return generate(sourceBytes, recipe.toString().getBytes());
   }
 
@@ -95,7 +99,7 @@ public class OpenApiGenerator {
    * Validates whether the recipe which contains other recipes is valid.
    * @throws Exception when it is not valid.
    */
-  private static void validateExtendedRecipe() throws Exception {
+  private static void validateRecipe() throws Exception {
     if (!recipe.has(RECIPES) || !recipe.get(RECIPES).isArray() || recipe.get(RECIPES).size() == 0) {
       throw new Exception("Invalid recipe. There must be a non-empty array named \"recipes\".");
     }
@@ -106,17 +110,17 @@ public class OpenApiGenerator {
         throw new Exception("Invalid recipe. There must be a name for each of the individual recipes.");
       }
 
-      RECIPES_NAMES.add(r.get(NAME).textValue());
+      RECIPES_MAP.put(r.get(NAME).textValue(), r);
     }
   }
 
   /**
    * Validates whether the name is contained within the recipes' names list.
-   * @param name the recipe name list.
+   * @param name the recipe name.
    * @throws Exception when the name is not in the list.
    */
   private static void validateRecipeName(final String name) throws Exception {
-    if (!RECIPES_NAMES.contains(name)) {
+    if (!RECIPES_MAP.containsKey(name)) {
       throw new Exception("Invalid recipe. Recipe name \"" + name + " not found.");
     }
   }
@@ -129,7 +133,7 @@ public class OpenApiGenerator {
     final ArrayNode recipes = (ArrayNode) recipe.get(RECIPES);
     for (JsonNode recipe : recipes) {
       if (recipe.has(EXTENDS)) {
-        if (!recipe.get(EXTENDS).isTextual() || !RECIPES_NAMES.contains(recipe.get(EXTENDS).textValue())) {
+        if (!recipe.get(EXTENDS).isTextual() || !RECIPES_MAP.containsKey(recipe.get(EXTENDS).textValue())) {
           throw new Exception("Invalid recipe. Recipe's field \"extends\" must reference a valid recipe within the recipes' names list.");
         }
       }
@@ -165,9 +169,35 @@ public class OpenApiGenerator {
 
   /**
    * Extends the main recipe when needed and prepares it to be executed.
+   * @param name the recipe name to be prepared.
    */
-  private static void prepareRecipe() {
-    // TODO recipe = the extended recipe
+  private static void prepareRecipe(final String name) {
+    recipe = prepareRecursively(RECIPES_MAP.get(name));
+  }
+
+  /**
+   * Navigates through the recipes hierarchy concatenating the recipes operation.
+   * @param recipe the initial recipe.
+   * @return the final recipe containing instructions from all parent recipes.
+   */
+  private static JsonNode prepareRecursively(final JsonNode recipe) {
+    if (!recipe.has(EXTENDS)) return recipe;
+
+    final String extension = recipe.get(EXTENDS).textValue();
+    final JsonNode parent = prepareRecursively(RECIPES_MAP.get(extension));
+
+    elements(parent).entrySet().stream().filter(entry -> entry.getValue().isArray()).forEach(entry -> {
+      final String key = (String) entry.getKey();
+      final ArrayNode array = (ArrayNode) entry.getValue();
+
+      if (recipe.has(key)) {
+        array.addAll((ArrayNode) recipe.get(key));
+      }
+
+      ((ObjectNode) recipe).set(key, array);
+    });
+
+    return recipe;
   }
 
   /**
@@ -274,10 +304,10 @@ public class OpenApiGenerator {
   }
 
   /**
-   * Iterates over the list of replacements and replaces the specified "find" element with the "replace" element.
+   * Iterates over the list of replacements and replaces the specified "path" element with the "replace" element.
    * Replacements can either be of types "key" or "value".
-   * For "key" type, the last element in the path string is renamed to the value of "replace" field.
-   * For "value" type, the child of the last element in the path string is replaced by the value of "replace" field.
+   * When "type" is of type value, it accepts path=* which replaces strings over the entire source object.
+   * Otherwise, executes replacement by matching the node's path.
    * @throws Exception in case of any error related to the replacement process.
    */
   private static void processReplacements() throws Exception {
@@ -287,49 +317,105 @@ public class OpenApiGenerator {
     while (it.hasNext()) {
       final JsonNode e = it.next();
       final String type = e.get(TYPE).asText();
-      final String find = e.get(FIND).asText();
-      JsonNode replace = e.get(REPLACE);
+      final String path = e.get(PATH).asText();
+      final JsonNode find = e.get(FIND);
+      final JsonNode replace = e.get(REPLACE);
 
-      final String[] pathKeys = split(find);
-      final String lastKey = pathKeys[pathKeys.length-1];
-      JsonNode parent = root;
-
-      // navigate to the last node's parent
-      for (int i=0; i<pathKeys.length-1; i++) {
-        parent = get(parent, pathKeys[i]);
+      if ("*".equals(path)) {
+        processStarReplacement(find, replace);
+        continue;
       }
 
-      if (parent == null) {
-        throw new Exception("Invalid path: " + find);
+      processPathReplacement(type, path, find, replace);
+    }
+  }
+
+  /**
+   * Simple string replacement.
+   * Executes the replacement across the entire source document by analysing any textual nodes.
+   * Finds the value of "find" within the values' text and replaces it with the value of "replace".
+   * e.g. when the replace recipe instruction contains path=*, find=space and replace=layer:
+   * the following text "Modify features in the space" becomes "Modify features in the layer"
+   * For more examples, see openapi-recipes.yaml
+   * @param find the text to be found within any textual node
+   * @param replace the text to be replaced in the place of "find" value.
+   */
+  private static void processStarReplacement(JsonNode find, JsonNode replace) {
+    final String searchString = find.textValue();
+    final String replacement = replace.textValue();
+
+    traverse(root, node ->
+      elements(node).forEach((k, v) -> {
+        if (v.isTextual() && v.textValue().contains(searchString)) {
+          String original = v.textValue();
+          set(node, String.valueOf(k), new TextNode(StringUtils.replace(original, searchString, replacement)));
+        }
+      })
+    );
+  }
+
+  /**
+   * Replaces strings and values by matching the node's path.
+   * For "key" type, the last element in the path string is renamed to the value of "replace" field.
+   * For "value" type, the child of the last element in the path string is replaced by the value of "replace" field.
+   * @param type the replacement type, either "key" or "value".
+   * @param path the path where the replacement will occur.
+   * @param find the string to be found within the value pointed by "path"
+   * @param replace the string replace the old value
+   * @throws Exception when the path is invalid
+   */
+  private static void processPathReplacement(String type, String path, JsonNode find, JsonNode replace) throws Exception {
+    final String[] pathKeys = split(path);
+    final String lastKey = pathKeys[pathKeys.length-1];
+    JsonNode parent = root;
+
+    // navigate to the last node's parent
+    for (int i=0; i<pathKeys.length-1; i++) {
+      parent = get(parent, pathKeys[i]);
+    }
+
+    if (parent == null) {
+      throw new Exception("Invalid path: " + path);
+    }
+
+    switch (type) {
+      case KEY: {
+        if (!parent.isObject()) break;
+
+        final String[] pathReplaceKeys = split(replace.textValue());
+        final String lastReplaceKey = pathReplaceKeys[pathReplaceKeys.length-1];
+
+        final ObjectNode objectNode = (ObjectNode) parent;
+        final JsonNode value = get(parent, lastKey);
+        objectNode.remove(lastKey);
+        objectNode.set(lastReplaceKey, value);
+
+        break;
       }
+      case VALUE: {
+        if (replace.isTextual()) {
+          String replacement = replace.textValue();
 
-      switch (type) {
-        case KEY: {
-          if (!parent.isObject()) break;
-
-          final String[] pathReplaceKeys = split(replace.asText());
-          final String lastReplaceKey = pathReplaceKeys[pathReplaceKeys.length-1];
-
-          final ObjectNode objectNode = (ObjectNode) parent;
-          final JsonNode value = get(parent, lastKey);
-          objectNode.remove(lastKey);
-          objectNode.set(lastReplaceKey, value);
-
-          break;
-        }
-        case VALUE: {
-          if (replace.isTextual() && replace.textValue().startsWith("${") && replace.textValue().endsWith("}")) {
-            replace = new TextNode(VALUES_MAP.get(StringUtils.substringBetween(replace.asText(), "${", "}")));
+          if (replacement.startsWith("${") && replacement.endsWith("}")) {
+            replacement = VALUES_MAP.get(StringUtils.substringBetween(replacement, "${", "}"));
           }
 
-          if (parent.isObject()) {
-            ((ObjectNode) parent).set(lastKey, replace);
-          } else if (parent.isArray()) {
-            ((ArrayNode) parent).set(Integer.parseInt(lastKey), replace);
+          boolean hasFind = find != null;
+          if (hasFind && parent.get(lastKey).isTextual()) {
+            final String original = parent.get(lastKey).textValue();
+            replacement = StringUtils.replace(original, find.textValue(), replacement);
           }
 
-          break;
+          replace = new TextNode(replacement);
         }
+
+        if (parent.isObject()) {
+          ((ObjectNode) parent).set(lastKey, replace);
+        } else if (parent.isArray()) {
+          ((ArrayNode) parent).set(Integer.parseInt(lastKey), replace);
+        }
+
+        break;
       }
     }
   }
@@ -405,6 +491,21 @@ public class OpenApiGenerator {
   }
 
   /**
+   * Sets a value into the parent object by checking whether it is an array or object, converting the key when it is necessary.
+   * @param parent the node in which the set will occur.
+   * @param key the node's key which will receive the new value.
+   * @param value the value to be set.
+   */
+  private static void set(JsonNode parent, String key, JsonNode value) {
+    if (parent == null) return;
+    if (parent.isObject()) {
+      ((ObjectNode) parent).set(key, value);
+    } else if (parent.isArray()) {
+      ((ArrayNode) parent).set(Integer.parseInt(key), value);
+    }
+  }
+
+  /**
    * Converts a JsonNode into a Map in which the keys are either Object's keys or Array's indices.
    * @param node the JsonNode object to be converted
    * @return A map of String|Integer-JsonNode. Empty if node is null
@@ -427,6 +528,21 @@ public class OpenApiGenerator {
   }
 
   /**
+   * Traverses the entire node hierarchy recursively and apply the function fn.
+   * @param node the node where the traverse will start.
+   * @param fn the fn to apply in each of the nodes.
+   */
+  private static void traverse(JsonNode node, Consumer<JsonNode> fn) {
+    if (node == null) return;
+    fn.accept(node);
+    if (node.isContainerNode()) {
+      for (JsonNode curr : node) {
+        traverse(curr, fn);
+      }
+    }
+  }
+
+  /**
    * Generates an open source specification by using a source and recipe files.
    * Results are saved into the output file.
    * e.g. <code>java com.here.xyz.hub.utilOpenApiGenerator &lt;source&gt; &lt;recipe&gt; &lt;recipe&gt;</code>
@@ -434,16 +550,27 @@ public class OpenApiGenerator {
    */
   public static void main(String... args) {
     try {
-      if (args.length != 3) throw new Exception("Invalid number of parameters.");
+      if (args.length < 3 || args.length > 5) throw new Exception("Invalid number of parameters.");
 
-      byte[] sourceBytes = FileUtils.readFileToByteArray(new File(args[0]));
-      byte[] recipeBytes = FileUtils.readFileToByteArray(new File(args[1]));
-      byte[] result = OpenApiGenerator.generate(sourceBytes, recipeBytes);
+      int i = 0;
+      if (args[i].equals("-d") || args[i].equals("--debug")) {
+        debug = true;
+        i++;
+      }
 
-      FileUtils.writeByteArrayToFile(new File(args[2]), result);
+      final byte[] sourceBytes = FileUtils.readFileToByteArray(new File(args[i++]));
+      final byte[] recipeBytes = FileUtils.readFileToByteArray(new File(args[i++]));
+      final boolean useName = args.length - (debug ? 1 : 0) == 4;
+      final byte[] result = useName ? generate(sourceBytes, recipeBytes, args[i]) : generate(sourceBytes, recipeBytes);
+
+      FileUtils.writeByteArrayToFile(new File(args[args.length-1]), result);
     } catch (Exception e) {
+      if (debug) {
+        e.printStackTrace();
+      }
+
       System.out.println(e.getMessage());
-      System.out.println("Usage: java OpenApiGenerator <source> <recipe> <output>");
+      System.out.println("Usage: java OpenApiGenerator [-d|--debug] <source> <recipe> [name] <output>");
     }
   }
 }
