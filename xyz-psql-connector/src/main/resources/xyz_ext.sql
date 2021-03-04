5dffe899602ef4ee2a9a8ad27e6b86f16e61c417
qGYdXbY2 <47661341+qGYdXbY2@users.noreply.github.com>
2021-03-04 14:34:03 +0100

    XYZ_EXT_VERSION = 138
    Signed-off-by: qGYdXbY2 <47661341+qGYdXbY2@users.noreply.github.com>

--- xyz-psql-connector/src/main/resources/xyz_ext.sql
+++ xyz-psql-connector/src/main/resources/xyz_ext.sql
@@ -151,7 +151,7 @@
 CREATE OR REPLACE FUNCTION xyz_ext_version()
   RETURNS integer AS
 $BODY$
- select 137
+ select 138
 $BODY$
   LANGUAGE sql IMMUTABLE;
 ------------------------------------------------
