043da85fc2fa4fdffe3eb0715fae258f816d0b38
Benjamin Rögner <benjamin.roegner@here.com>
2021-03-08 10:58:33 +0100

    Merge log-level change into vertx4 branch
    Signed-off-by: Benjamin Rögner <benjamin.roegner@here.com>

--- xyz-hub-service/src/main/java/com/here/xyz/hub/Service.java
+++ xyz-hub-service/src/main/java/com/here/xyz/hub/Service.java
@@ -61,6 +61,7 @@ import java.util.List;
 import java.util.Properties;
 import java.util.UUID;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.config.Configurator;
