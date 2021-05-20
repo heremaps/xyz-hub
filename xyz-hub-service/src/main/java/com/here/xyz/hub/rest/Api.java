39fa62f443f93ad743558ca6af6c14c93af78923
Benjamin Rögner <benjamin.roegner@here.com>
2021-05-20 14:11:54 +0200

    Set Strict-Transport-Security response-header on all responses
    Signed-off-by: Benjamin Rögner <benjamin.roegner@here.com>

--- xyz-hub-service/src/main/java/com/here/xyz/hub/rest/Api.java
+++ xyz-hub-service/src/main/java/com/here/xyz/hub/rest/Api.java
@@ -499,6 +499,7 @@ public abstract class Api {
 
     public static final String STREAM_ID = "Stream-Id";
     public static final String STREAM_INFO = "Stream-Info";
+    public static final String STRICT_TRANSPORT_SECURITY = "Strict-Transport-Security";
     public static final String APPLICATION_GEO_JSON = "application/geo+json";
     public static final String APPLICATION_JSON = "application/json";
     public static final String APPLICATION_VND_MAPBOX_VECTOR_TILE = "application/vnd.mapbox-vector-tile";
