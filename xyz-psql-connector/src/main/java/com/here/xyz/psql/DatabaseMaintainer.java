9c70513ab6349f85dbd6fc980df52dd7788c0cd2
Maximilian Chrzan <maximilian.chrzan@here.com>
2021-12-22 15:24:09 +0100

    Add minVersion to HistoryStatisticsResponse
    Signed-off-by: Maximilian Chrzan <maximilian.chrzan@here.com>

--- xyz-psql-connector/src/main/java/com/here/xyz/psql/DatabaseMaintainer.java
+++ xyz-psql-connector/src/main/java/com/here/xyz/psql/DatabaseMaintainer.java
@@ -47,7 +47,7 @@ public class DatabaseMaintainer {
     private static final Logger logger = LogManager.getLogger();
 
     /** Is used to check against xyz_ext_version() */
-    public static final int XYZ_EXT_VERSION = 145;
+    public static final int XYZ_EXT_VERSION = 146;
 
     public static final int H3_CORE_VERSION = 107;
 
