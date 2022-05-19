c862dffe7ff3654f4f5aaa7402a141af8bcb8f88
Alexander Lowey - Weber <alexander.weber@here.com>
2022-05-19 15:03:30 +0200

    Implemented a JSON config file and env reader, used later tofix Rest-Assured tests.
    Signed-off-by: Alexander Lowey - Weber <alexander.weber@here.com>

---
+++ xyz-models/src/main/java/com/here/xyz/util/JsonConfigFile.java
@@ -0,0 +1,337 @@
+package com.here.xyz.util;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.jar.JarFile;
+import java.util.zip.ZipEntry;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+/**
+ * Base class of a configuration file that is read from a JSON file with optional override via environment variables. The environment
+ * variables are all required to be upper-cased and are optionally prefixed by a defined string. The configuration file should contain the
+ * member names in exact notation, unless annotated.
+ *
+ * @param <SELF> this type.
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public abstract class JsonConfigFile<SELF extends JsonConfigFile<SELF>> {
+
+  /**
+   * Returns the name of the environment variable that should be used to define the search path for the configuration files.
+   * <p>
+   * Optionally it is possible to add an {@link EnvName} annotation to the class, then this defines the name of the environment variable
+   * that holds the search path.
+   * <p>
+   * In a nutshell, by default the method returns {@code XYZ_CONFIG_PATH}, if the {@link EnvName} annotation is found at the given class
+   * type, for example {@code @EnvName("MY_CONFIG_PATH")}, then the method returns exactly this: {@code MY_CONFIG_PATH}.
+   *
+   * @param classType the class for which to check the annotations.
+   * @return the name of the environment variable that holds the config file search path.
+   */
+  @Nonnull
+  public static String configPathEnvName(@Nullable Class<?> classType) {
+    while (classType != null) {
+      if (classType.isAnnotationPresent(EnvName.class)) {
+        final EnvName[] envNames = classType.getAnnotationsByType(EnvName.class);
+        return envNames[0].value();
+      }
+      classType = classType.getSuperclass();
+    }
+    return "XYZ_CONFIG_PATH";
+  }
+
+  /**
+   * An optional prefix to be placed in-front of all field names to be read from environment variables. So, the prefix "FOO_" will cause the
+   * property "bar" to be looked-up as "FOO_BAR". If no prefix is returned, the environment variable name must explicitly be declared using
+   * the annotation {@link EnvName}. Multiple names can be defined and combined with the prefix.
+   *
+   * @return a prefix or null.
+   */
+  @Nullable
+  protected String envPrefix() {
+    return null;
+  }
+
+  /**
+   * Return the default file name.
+   *
+   * @return the default file name; null if no such default exists.
+   */
+  @Nullable
+  abstract protected String defaultFile();
+
+  /**
+   * A helper method to detect pseudo null values in environment variables. Without this, you can never undefine (unset) a pre-defined value
+   * via environment variable. To allow this, this method treats an empty string and the string "null" as null (undefine).
+   *
+   * @param string the string that is read from the environment variable.
+   * @return null if the value represents null; otherwise the given value.
+   */
+  @Nullable
+  public static String nullable(@Nullable String string) {
+    return string == null || string.isEmpty() || "null".equalsIgnoreCase(string) ? null : string;
+  }
+
+  /**
+   * Used for Jackson to parse a JSON object into a map.
+   */
+  private static final TypeReference<Map<String, Object>> MAP_TYPE = new TypeReference<Map<String, Object>>() {
+  };
+
+  /**
+   * Load the configuration from a disk file, a file in the resources (JAR) or from environment variables. Basically the order is to first
+   * read from a disk file, if not found, read from resources. In both cases, eventually the environment variables will override all values
+   * being read from the configuration file.
+   *
+   * @return this.
+   * @throws IOException if any error occurred while reading from disk or resources.
+   */
+  public final SELF load() throws IOException {
+    return load(System::getenv);
+  }
+
+  /**
+   * Load the configuration from a disk file, a file in the resources (JAR) or from environment variables. Basically the order is to first
+   * read from a disk file, if not found, read from resources. In both cases, eventually the environment variables will override all values
+   * being read from the configuration file.
+   *
+   * @param getEnv the reference to the method that reads environment variables.
+   * @return this.
+   * @throws IOException          if any error occurred while reading from disk or resources.
+   * @throws NullPointerException if getEnv is null.
+   */
+  public final SELF load(final @Nonnull Function<String, String> getEnv) throws IOException {
+    Map<String, Object> configValues = null;
+
+    final String configPath = nullable(getEnv.apply(configPathEnvName(getClass())));
+    final String configFile = nullable(defaultFile());
+    if (configFile != null) {
+      String filePath;
+      try {
+        final Path path = (configPath != null ? Paths.get(configPath, configFile) : Paths.get(configFile)).toAbsolutePath();
+        final File file = path.toFile();
+        if (file.exists() && file.isFile()) {
+          filePath = path.toString();
+        } else {
+          // Try to load from resources (JAR).
+          URL url = getClass().getResource(configFile);
+          if (url == null && !configFile.startsWith("/")) {
+            url = getClass().getResource("/" + configFile);
+          }
+          filePath = url != null ? url.getPath() : null;
+        }
+      } catch (final Throwable t) {
+        error("Failed to load config file: " + configFile, t);
+        filePath = null;
+      }
+
+      if (filePath != null) {
+        try {
+          final InputStream in;
+          final int jarEnd = filePath.indexOf(".jar!");
+          if (filePath.startsWith("file:") && jarEnd > 0) {
+            final String jarPath = filePath.substring("file:".length(), jarEnd + ".jar".length());
+            filePath = filePath.substring(jarEnd + ".jar!".length());
+            if (filePath.startsWith("/")) {
+              filePath = filePath.substring(1);
+            }
+            info("Load configuration file from JAR, jar-path: " + jarPath + ", file-path: " + filePath);
+            try (final JarFile jarFile = new JarFile(jarPath)) {
+              final ZipEntry entry = jarFile.getEntry(filePath);
+              if (entry != null && entry.isDirectory()) {
+                throw new IOException("Config file is directory");
+              }
+              if (entry == null) {
+                throw new FileNotFoundException(filePath);
+              }
+              in = jarFile.getInputStream(entry);
+            }
+          } else {
+            info("Load configuration file from disk: " + filePath);
+            in = Files.newInputStream(new File(filePath).toPath());
+          }
+          try {
+            configValues = new ObjectMapper().readValue(in, MAP_TYPE);
+          } finally {
+            in.close();
+          }
+        } catch (Throwable t) {
+          error("Failed to load configuration file: " + configFile, t);
+        }
+      }
+    }
+    return load(configValues, getEnv);
+  }
+
+  /**
+   * Read the configuration from the given map and eventually use environment variables to override values.
+   *
+   * @param defaultValues the map to load values from.
+   * @return this.
+   */
+  @Nonnull
+  public final SELF load(@Nonnull Map<String, Object> defaultValues) {
+    return load(defaultValues, System::getenv);
+  }
+
+  /**
+   * Read the configuration from a given map and eventually use environment variables to override all values being read from the map.
+   *
+   * @param configValues the configuration values to use.
+   * @param getEnv       the reference to the method that reads the environment variable; if null, then no environment variables are read.
+   * @return this.
+   */
+  @Nonnull
+  public final SELF load(
+      @Nullable Map<String, Object> configValues, @Nullable Function<String, String> getEnv) {
+    final String prefix;
+    final int prefixLen;
+    final StringBuilder sb;
+    if (getEnv != null) {
+      sb = new StringBuilder(128);
+      prefix = envPrefix();
+      if (prefix != null) {
+        prefixLen = prefix.length();
+        for (int i = 0; i < prefix.length(); i++) {
+          sb.append(Character.toUpperCase(prefix.charAt(i)));
+        }
+      } else {
+        prefixLen = 0;
+      }
+    } else {
+      prefix = null;
+      prefixLen = 0;
+      sb = null;
+    }
+    Class<?> theClass = getClass();
+    do {
+      for (final Field field : theClass.getDeclaredFields()) {
+        String name = field.getName();
+        // @JsonProperty will rename the property.
+        if (field.isAnnotationPresent(JsonProperty.class)) {
+          name = field.getAnnotation(JsonProperty.class).value();
+        }
+
+        if (configValues != null) {
+          // If the property exists under its correct name, use this.
+          if (configValues.containsKey(name)) {
+            setField(field, configValues.get(name));
+          } else {
+            // Try all alternative names in the order in which they are annotated.
+            if (field.isAnnotationPresent(JsonName.class)) {
+              final JsonName[] jsonNames = field.getAnnotationsByType(JsonName.class);
+              for (final JsonName jsonName : jsonNames) {
+                if (configValues.containsKey(jsonName.value())) {
+                  setField(field, configValues.get(jsonName.value()));
+                  break;
+                }
+              }
+            }
+          }
+        }
+        if (getEnv != null) {
+          if (prefix != null) {
+            setFromEnv(sb, prefixLen, field, name, true, getEnv);
+          }
+          if (field.isAnnotationPresent(EnvName.class)) {
+            final EnvName[] envNames = field.getAnnotationsByType(EnvName.class);
+            for (final EnvName envName : envNames) {
+              setFromEnv(sb, prefixLen, field, envName.value(), envName.prefix(), getEnv);
+            }
+          }
+          // If the field is not annotated and no prefix is defined,
+          // then we do not read the value from the environment!
+        }
+      }
+      theClass = theClass.getSuperclass();
+    } while (theClass != null);
+    //noinspection unchecked
+    return (SELF) this;
+  }
+
+  private void setFromEnv(
+      @Nonnull StringBuilder sb,
+      int prefixLen,
+      @Nonnull Field field,
+      @Nonnull String name,
+      boolean withPrefix,
+      @Nonnull Function<String, String> getEnv) {
+    sb.setLength(prefixLen);
+    for (int i = 0; i < name.length(); i++) {
+      sb.append(Character.toUpperCase(name.charAt(i)));
+    }
+    final String envName = withPrefix ? sb.toString() : sb.substring(prefixLen, sb.length());
+    final String envValue = getEnv.apply(envName);
+    if (envValue != null) {
+      setField(field, envValue);
+    }
+  }
+
+  private void setField(@Nonnull Field field, @Nullable Object value) {
+    if (value instanceof String) {
+      value = nullable((String) value);
+    }
+    try {
+      final Class<?> fieldType = field.getType();
+      if (fieldType == int.class) {
+        if (value instanceof String) {
+          final int intValue = Integer.parseInt((String) value, 10);
+          field.setInt(this, intValue);
+        } else if (value instanceof Number) {
+          field.setInt(this, ((Number) value).intValue());
+        }
+      } else if (fieldType == String.class) {
+        if (value == null) {
+          field.set(this, null);
+        } else if (value instanceof String) {
+          field.set(this, value);
+        } else if (value instanceof Number) {
+          field.set(this, value.toString());
+        }
+      } else if (fieldType == boolean.class) {
+        if (value instanceof String) {
+          final String stringValue = (String) value;
+          final boolean boolValue =
+              !"false".equalsIgnoreCase(stringValue) && !"null".equalsIgnoreCase(stringValue);
+          field.setBoolean(this, boolValue);
+        } else if (value instanceof Number) {
+          field.setBoolean(this, ((Number) value).intValue() != 0);
+        } else if (value instanceof Boolean) {
+          field.setBoolean(this, (Boolean) value);
+        }
+      }
+    } catch (NullPointerException | NumberFormatException | IllegalAccessException ignore) {
+    }
+  }
+
+  /**
+   * A method that can be overridden to send information to a logger.
+   *
+   * @param message the message to log as information.
+   */
+  protected void info(String message) {
+  }
+
+  /**
+   * A method that can be overridden to send errors to a logger.
+   *
+   * @param message the message to log.
+   * @param cause   the cause.
+   */
+  protected void error(String message, Throwable cause) {
+  }
+}
---
+++ xyz-models/src/test/java/com/here/xyz/util/JsonConfigFileTest.java
@@ -0,0 +1,231 @@
+package com.here.xyz.util;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.LinkedHashMap;
+import javax.annotation.Nullable;
+import org.junit.Test;
+
+@SuppressWarnings("DuplicatedCode")
+public class JsonConfigFileTest {
+
+  private static final String test_config_file = "/test_config_file.json";
+  private static final String test_config_file2 = "/test_config_file2.json";
+
+  @EnvName("TEST_CONFIG_PATH")
+  private static class TestConfig extends JsonConfigFile<TestConfig> {
+
+    protected void info(String message) {
+      System.out.println(message);
+    }
+
+    protected void error(String message, Throwable t) {
+      System.err.println(message);
+      if (t != null) {
+        t.printStackTrace(System.err);
+      }
+    }
+
+    @Nullable
+    @Override
+    protected String defaultFile() {
+      return test_config_file;
+    }
+
+    @Nullable
+    @Override
+    protected String envPrefix() {
+      return "TEST_";
+    }
+
+    public int theInt = 1;
+    public boolean theBool = false;
+    public String theString = "foo";
+    public String env = "env-default";
+  }
+
+  public static class TestConfig2 extends TestConfig {
+
+    @Nullable
+    @Override
+    protected String defaultFile() {
+      return test_config_file2;
+    }
+
+  }
+
+  public static class TestConfigWithAnnotation extends TestConfig {
+
+    @EnvName(value = "BAR", prefix = true)
+    public String testAnnotation = "foo";
+  }
+
+  public static class TestConfigWithAnnotationWithoutPrefix extends TestConfig {
+
+    @EnvName("BAR")
+    public String testAnnotation = "foo";
+  }
+
+  @Test
+  public void test_defaults() {
+    final TestConfig testConfig = new TestConfig();
+    assertEquals(1, testConfig.theInt);
+    assertFalse(testConfig.theBool);
+    assertEquals("foo", testConfig.theString);
+    assertEquals("env-default", testConfig.env);
+  }
+
+  @Test
+  public void test_withMap() {
+    final TestConfig testConfig = new TestConfig();
+    final LinkedHashMap<String, Object> testValues = new LinkedHashMap<>();
+    testValues.put("theInt", 5);
+    testValues.put("theBool", true);
+    testValues.put("theString", "string");
+    testValues.put("env", "env");
+    testConfig.load(testValues, null);
+    assertEquals(5, testConfig.theInt);
+    assertTrue(testConfig.theBool);
+    assertEquals("string", testConfig.theString);
+    assertEquals("env", testConfig.env);
+  }
+
+  @Nullable
+  private static String test_withMapAndEnv_getEnv(String key) {
+    if ("TEST_ENV".equals(key)) {
+      return "env-overridden";
+    }
+    if ("TEST_THESTRING".equals(key)) {
+      return "null";
+    }
+    return null;
+  }
+
+  @Test
+  public void test_withMapAndEnv() {
+    final TestConfig testConfig = new TestConfig();
+    final LinkedHashMap<String, Object> testValues = new LinkedHashMap<>();
+    testValues.put("theInt", 5);
+    testValues.put("theBool", true);
+    testValues.put("theString", "string");
+    testValues.put("env", "env");
+    testConfig.load(testValues, JsonConfigFileTest::test_withMapAndEnv_getEnv);
+    assertEquals(5, testConfig.theInt);
+    assertTrue(testConfig.theBool);
+    assertNull(testConfig.theString);
+    assertEquals("env-overridden", testConfig.env);
+  }
+
+  @Nullable
+  private static String test_config_file_getEnv(String key) {
+    return null;
+  }
+
+  @Test
+  public void test_config_file() throws Exception {
+    final TestConfig testConfig = new TestConfig();
+    testConfig.load(JsonConfigFileTest::test_config_file_getEnv);
+    assertEquals(100, testConfig.theInt);
+    assertTrue(testConfig.theBool);
+    assertEquals("Hello World!", testConfig.theString);
+    assertEquals("default", testConfig.env);
+  }
+
+  @Nullable
+  private static String test_env_getEnv(String key) {
+    if ("TEST_ENV".equals(key)) {
+      return "noDefault";
+    }
+    if ("TEST_THEINT".equals(key)) {
+      return "10";
+    }
+    if ("TEST_THEBOOL".equals(key)) {
+      return "false";
+    }
+    if ("TEST_THESTRING".equals(key)) {
+      return "boobar";
+    }
+    return null;
+  }
+
+  @Test
+  public void test_env() throws Exception {
+    final TestConfig testConfig = new TestConfig();
+    testConfig.load(JsonConfigFileTest::test_env_getEnv);
+    assertEquals(10, testConfig.theInt);
+    assertFalse(testConfig.theBool);
+    assertEquals("boobar", testConfig.theString);
+    assertEquals("noDefault", testConfig.env);
+  }
+
+  @Nullable
+  private static String test_config_file2_getEnv(String key) {
+    if ("TEST_CONFIG_PATH".equals(key)) {
+      return "/";
+    }
+    return null;
+  }
+
+  @Test
+  public void test_config_file2() throws Exception {
+    final TestConfig2 testConfig = new TestConfig2();
+    testConfig.load(JsonConfigFileTest::test_config_file2_getEnv);
+    assertEquals(-100, testConfig.theInt);
+    assertTrue(testConfig.theBool);
+    assertEquals("Hello World!", testConfig.theString);
+    assertEquals("default", testConfig.env);
+  }
+
+  @Nullable
+  private static String test_nullable_getEnv(String key) {
+    if ("TEST_THESTRING".equals(key)) {
+      return "null";
+    }
+    return null;
+  }
+
+  @Test
+  public void test_nullable() throws Exception {
+    final TestConfig testConfig = new TestConfig();
+    testConfig.load(JsonConfigFileTest::test_nullable_getEnv);
+    assertEquals(100, testConfig.theInt);
+    assertTrue(testConfig.theBool);
+    assertNull(testConfig.theString);
+    assertEquals("default", testConfig.env);
+  }
+
+  @Nullable
+  private static String test_annotationWithPrefix_getEnv(String key) {
+    if ("TEST_BAR".equals(key)) {
+      return "bar";
+    }
+    return null;
+  }
+
+  @Test
+  public void test_annotationWithPrefix() throws Exception {
+    final TestConfigWithAnnotation testConfig = new TestConfigWithAnnotation();
+    testConfig.load(JsonConfigFileTest::test_annotationWithPrefix_getEnv);
+    assertEquals("bar", testConfig.testAnnotation);
+  }
+
+  @Nullable
+  private static String test_annotationWithoutPrefix_getEnv(String key) {
+    if ("BAR".equals(key)) {
+      return "bar";
+    }
+    return null;
+  }
+
+  @Test
+  public void test_annotationWithoutPrefix() throws Exception {
+    final TestConfigWithAnnotationWithoutPrefix testConfig = new TestConfigWithAnnotationWithoutPrefix();
+    testConfig.load(JsonConfigFileTest::test_annotationWithoutPrefix_getEnv);
+    assertEquals("bar", testConfig.testAnnotation);
+  }
+
+}
