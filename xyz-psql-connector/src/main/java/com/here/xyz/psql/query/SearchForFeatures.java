56217eed02e97a5f362fa739cd287e8f41078525
Maximilian Chrzan <maximilian.chrzan@here.com>
2022-09-22 17:45:50 +0200

    Merge with master
    Signed-off-by: Maximilian Chrzan <maximilian.chrzan@here.com>

--- xyz-psql-connector/src/main/java/com/here/xyz/psql/query/SearchForFeatures.java
+++ xyz-psql-connector/src/main/java/com/here/xyz/psql/query/SearchForFeatures.java
@@ -43,7 +43,7 @@ public class SearchForFeatures<E extends SearchForFeaturesEvent> extends Extende
   protected boolean hasSearch;
 
   public SearchForFeatures(E event, DatabaseHandler dbHandler) throws SQLException {
-    super(event, dbHandler);
+    super(event, dbHandler,false);
   }
 
   @Override
