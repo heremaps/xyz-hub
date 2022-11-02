b75007f34c5951ef3e23b84b59087bef8fe71c5d
Maximilian Chrzan <maximilian.chrzan@here.com>
2022-11-02 09:25:29 +0100

    Add aws_s3 extension.
    Signed-off-by: Maximilian Chrzan <maximilian.chrzan@here.com>

--- xyz-psql-connector/src/main/resources/xyz_ext.sql
+++ xyz-psql-connector/src/main/resources/xyz_ext.sql
@@ -153,11 +153,10 @@ DROP FUNCTION IF EXISTS xyz_statistic_history(text, text);
 CREATE OR REPLACE FUNCTION xyz_ext_version()
   RETURNS integer AS
 $BODY$
- select 148
+ select 149
 $BODY$
   LANGUAGE sql IMMUTABLE;
-------------------------------------------------
-------------------------------------------------
+----------
 -- ADD NEW COLUMN TO IDX_STATUS TABLE
 DO $$
 BEGIN
@@ -167,6 +166,27 @@ EXCEPTION
 	    WHEN duplicate_column THEN RAISE NOTICE 'column <auto_indexing> already exists in <xyz_idxs_status>.';
 END;
 $$;
+------------------------------------------------
+------------------------------------------------
+CREATE OR  REPLACE FUNCTION xyz_random_string(length integer)
+    RETURNS text AS
+$BODY$
+    DECLARE
+        chars text[] := '{0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}';
+        result text := '';
+        i integer := 0;
+    BEGIN
+            IF length < 0 THEN
+                RAISE EXCEPTION 'Given length cannot be less than 0';
+        END IF;
+        FOR i IN 1..length LOOP
+            result := result || chars[1+random()*(array_length(chars, 1)-1)];
+        END LOOP;
+
+        RETURN result;
+    END;
+$BODY$
+LANGUAGE plpgsql VOLATILE;
 ------------------------------------------------
 ------------------------------------------------
 -- Function: xyz_index_dissolve_datatype(text)
