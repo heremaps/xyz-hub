078ea73265279eb5c2d5e9e3880a3609307dc5da
Benjamin Rögner <benjamin.roegner@here.com>
2023-01-11 14:57:47 +0100

    Fix storage statistics QR to work correctly with partitioning
    Signed-off-by: Benjamin Rögner <benjamin.roegner@here.com>

--- xyz-psql-connector/src/main/java/com/here/xyz/psql/query/GetStorageStatistics.java
+++ xyz-psql-connector/src/main/java/com/here/xyz/psql/query/GetStorageStatistics.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017-2022 HERE Europe B.V.
+ * Copyright (C) 2017-2023 HERE Europe B.V.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,7 +24,6 @@ import static com.here.xyz.psql.DatabaseHandler.HISTORY_TABLE_SUFFIX;
 import com.here.xyz.connectors.ErrorResponseException;
 import com.here.xyz.events.GetStorageStatisticsEvent;
 import com.here.xyz.psql.DatabaseHandler;
-import com.here.xyz.psql.QueryRunner;
 import com.here.xyz.psql.SQLQuery;
 import com.here.xyz.responses.StatisticsResponse.Value;
 import com.here.xyz.responses.StorageStatistics;
@@ -60,7 +59,6 @@ public class GetStorageStatistics extends XyzQueryRunner<GetStorageStatisticsEve
     event.getSpaceIds().forEach(spaceId -> {
       String tableName = resolveTableName(spaceId);
       tableNames.add(tableName);
-      tableNames.add(tableName + HISTORY_TABLE_SUFFIX);
     });
     return new SQLQuery( "SELECT relname                                                AS " + TABLE_NAME + ","
                             + "       pg_indexes_size(c.oid)                                 AS " + INDEX_BYTES + ","
@@ -69,10 +67,10 @@ public class GetStorageStatistics extends XyzQueryRunner<GetStorageStatisticsEve
                             + "         LEFT JOIN pg_namespace n ON n.oid = c.relnamespace "
                             + " WHERE relkind = 'r'"
                             + " AND nspname = '" + getSchema() + "'"
-                            + " AND relname IN (" + tableNames
+                            + " AND relname LIKE ANY (array[" + tableNames
                                                       .stream()
-                                                      .map(tableName -> "'" + tableName + "'")
-                                                      .collect(Collectors.joining(",")) + ")");
+                                                      .map(tableName -> "'" + tableName + "%'") //TODO: replace % by _% once all tables have been migrated to new partitioned table style
+                                                      .collect(Collectors.joining(",")) + "])");
   }
 
   private String resolveTableName(String spaceId) {
@@ -94,15 +92,16 @@ public class GetStorageStatistics extends XyzQueryRunner<GetStorageStatisticsEve
     //Read the space / history info from the returned ResultSet
     while (rs.next()) {
       String tableName = rs.getString(TABLE_NAME);
-      boolean isHistoryTable = tableName.endsWith(HISTORY_TABLE_SUFFIX);
-      tableName = isHistoryTable ? tableName.substring(0, tableName.length() - HISTORY_TABLE_SUFFIX.length()) : tableName;
+      int suffixPos = tableName.lastIndexOf('_');
+      //TODO: The following is a backwards-compatibility implementation for the old table style and can be removed once all tables have been migrated to the new partitioned table style
+      tableName = suffixPos != -1 ? tableName.substring(0, suffixPos) : tableName;
       String spaceId = tableName2SpaceId.containsKey(tableName) ? tableName2SpaceId.get(tableName) : tableName;
 
       long tableBytes = rs.getLong(TABLE_BYTES),
            indexBytes = rs.getLong(INDEX_BYTES);
 
       SpaceByteSizes sizes = byteSizes.computeIfAbsent(spaceId, k -> new SpaceByteSizes());
-      if (isHistoryTable)
+      if (isHistoryTable(tableName))
         sizes.setHistoryBytes(new Value<>(tableBytes + indexBytes).withEstimated(true));
       else {
         sizes.setContentBytes(new Value<>(tableBytes).withEstimated(true));
@@ -116,5 +115,15 @@ public class GetStorageStatistics extends XyzQueryRunner<GetStorageStatisticsEve
           .withContentBytes(new Value<>(0L).withEstimated(true)));
 
     return stats;
+  }
+
+  private static boolean isHistoryTable(String tableName) {
+    int suffixPos = tableName.lastIndexOf('_');
+    if (suffixPos == -1)
+      return false;
+    String suffix = tableName.substring(suffixPos);
+    if (suffix.startsWith("_p"))
+      return true;
+    return tableName.endsWith(HISTORY_TABLE_SUFFIX);
   }
 }
