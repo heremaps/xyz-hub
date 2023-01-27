3338677e74f25c524d44674412c8644004a15463
Lucas Ceni <lucas.ceni@here.com>
2023-01-27 11:04:58 +0100

    Do not fill uuid and puuid when v2k>1
    Signed-off-by: Lucas Ceni <lucas.ceni@here.com>

--- xyz-hub-service/src/main/java/com/here/xyz/hub/task/FeatureTaskHandler.java
+++ xyz-hub-service/src/main/java/com/here/xyz/hub/task/FeatureTaskHandler.java
@@ -1282,7 +1282,7 @@ public class FeatureTaskHandler {
     nsXyz.setUpdatedAt(now);
 
     // UUID fields
-    if (task.space.isEnableUUID()) {
+    if (task.space.isEnableUUID() && task.space.getVersionsToKeep() <= 1) {
       nsXyz.setUuid(UUID.randomUUID().toString());
 
       if (!isInsert) {
