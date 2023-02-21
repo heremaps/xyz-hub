91848a757844763a55ed6ae088003eadc41ed10f
qGYdXbY2 <47661341+qGYdXbY2@users.noreply.github.com>
2023-02-21 12:09:59 +0100

    quadbin: sql optimization (#875)
    * quadbin: sql optimization
    
    Signed-off-by: qGYdXbY2 <47661341+qGYdXbY2@users.noreply.github.com>
    
    * hexbin: sql optimization
    
    Signed-off-by: qGYdXbY2 <47661341+qGYdXbY2@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: qGYdXbY2 <47661341+qGYdXbY2@users.noreply.github.com>

--- xyz-psql-connector/src/main/java/com/here/xyz/psql/SQLQuery.java
+++ xyz-psql-connector/src/main/java/com/here/xyz/psql/SQLQuery.java
@@ -20,6 +20,7 @@
 package com.here.xyz.psql;
 
 import static com.here.xyz.psql.DatabaseHandler.HISTORY_TABLE_SUFFIX;
+import static com.here.xyz.psql.DatabaseHandler.HEAD_TABLE_SUFFIX;
 
 import com.here.xyz.events.PropertyQuery;
 import com.here.xyz.events.QueryEvent;
@@ -53,6 +54,7 @@ public class SQLQuery {
   private static final String FRAGMENT_SUFFIX = "}}";
   private static final String VAR_SCHEMA = "schema";
   private static final String VAR_TABLE = "table";
+  private static final String VAR_TABLE_HEAD = "table_head";
   private static final String VAR_HST_TABLE = "hsttable";
   private static final String VAR_TABLE_SEQ = "table_seq";
   private static final String VAR_HST_TABLE_SEQ = "hsttable_seq";
@@ -107,7 +109,7 @@ public class SQLQuery {
     return result;
   }
 
-  @Deprecated
+  //@Deprecated
   public void append(String text, Object... parameters) {
     addText(text);
     addParameters(parameters);
@@ -243,6 +245,7 @@ public class SQLQuery {
     SQLQuery q = new SQLQuery(query)
         .withVariable(VAR_SCHEMA, schema)
         .withVariable(VAR_TABLE, table)
+        .withVariable(VAR_TABLE_HEAD, table + HEAD_TABLE_SUFFIX )
         .withVariable(VAR_HST_TABLE, table + HISTORY_TABLE_SUFFIX)
         .withVariable(VAR_TABLE_SEQ, table != null ? table.replaceAll("-", "_") + "_i_seq\";" : "")
         .withVariable(VAR_HST_TABLE_SEQ, table != null ? (table + HISTORY_TABLE_SUFFIX + "_seq").replaceAll("-", "_") : " ");
