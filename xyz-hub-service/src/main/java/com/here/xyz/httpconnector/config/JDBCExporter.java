1b59c3ef3676b449d15f8e9addc2e7f012ea843b
qGYdXbY2 <47661341+qGYdXbY2@users.noreply.github.com>
2023-06-12 17:31:10 +0200

    s3 export: fix NPE - VML failing for region with no data
    Signed-off-by: qGYdXbY2 <47661341+qGYdXbY2@users.noreply.github.com>

--- xyz-hub-service/src/main/java/com/here/xyz/httpconnector/config/JDBCExporter.java
+++ xyz-hub-service/src/main/java/com/here/xyz/httpconnector/config/JDBCExporter.java
@@ -187,6 +187,8 @@ public class JDBCExporter extends JDBCClients{
                 .map(row -> {
                     Row res = row.iterator().next();
                     if (res != null) {
+                      String[] sArr = res.getArrayOfStrings("tilelist");
+                      if( sArr != null && sArr.length > 0)
                         return Arrays.stream(res.getArrayOfStrings("tilelist")).collect(Collectors.toList());
                     }
                     return null;
