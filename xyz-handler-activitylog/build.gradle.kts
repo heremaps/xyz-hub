84c0235e98219af0f50c651d4db1b86ed503984d
Alexander Lowey - Weber <alexander.weber@here.com>
2023-06-20 12:30:17 +0200

    Cleanup activity-log build.

--- xyz-handler-activitylog/build.gradle.kts
+++ xyz-handler-activitylog/build.gradle.kts
@@ -1,12 +1,8 @@
 dependencies {
     implementation(project(":xyz-lib-models"))
     implementation(project(":xyz-lib-psql"))
-    implementation("com.flipkart.zjsonpatch:zjsonpatch:0.4.8")
-    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.17.1")
-    implementation("org.apache.logging.log4j:log4j-core:2.17.1")
-    implementation("org.apache.logging.log4j:log4j-api:2.17.1")
-    implementation("org.apache.logging.log4j:log4j-jcl:2.17.1")
-    implementation("com.vividsolutions:jts-core:1.14.0")
+
+    implementation("com.flipkart.zjsonpatch:zjsonpatch:0.4.13")
     testImplementation("com.jayway.jsonpath:json-path:2.7.0")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.2")
 }
