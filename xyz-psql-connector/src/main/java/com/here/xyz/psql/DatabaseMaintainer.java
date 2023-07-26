6ecb6600d92c82ba43d49c8f56e871f50b929c05
qGYdXbY2 <47661341+qGYdXbY2@users.noreply.github.com>
2023-07-26 16:34:13 +0200

    quadbin: use st_tileEnvelope (#982)
    Signed-off-by: qGYdXbY2 <47661341+qGYdXbY2@users.noreply.github.com>

--- xyz-psql-connector/src/main/java/com/here/xyz/psql/DatabaseMaintainer.java
+++ xyz-psql-connector/src/main/java/com/here/xyz/psql/DatabaseMaintainer.java
@@ -49,7 +49,7 @@ public class DatabaseMaintainer {
     private static final Logger logger = LogManager.getLogger();
 
     /** Is used to check against xyz_ext_version() */
-    public static final int XYZ_EXT_VERSION = 172;
+    public static final int XYZ_EXT_VERSION = 173;
 
     public static final int H3_CORE_VERSION = 108;
 
