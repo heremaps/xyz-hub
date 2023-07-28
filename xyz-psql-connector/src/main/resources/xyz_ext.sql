a52af874ca1a983d96c3441c009e4a8ad424208d
qGYdXbY2 <47661341+qGYdXbY2@users.noreply.github.com>
2023-07-28 12:14:34 +0200

    export sql: exp_qk_weight() - fix divide by zero error
    Signed-off-by: qGYdXbY2 <47661341+qGYdXbY2@users.noreply.github.com>

--- xyz-psql-connector/src/main/resources/xyz_ext.sql
+++ xyz-psql-connector/src/main/resources/xyz_ext.sql
@@ -111,7 +111,7 @@ DROP FUNCTION IF EXISTS exp_build_sql_inhabited_txt(boolean, text, integer, text
 CREATE OR REPLACE FUNCTION xyz_ext_version()
   RETURNS integer AS
 $BODY$
- select 173
+ select 174
 $BODY$
   LANGUAGE sql IMMUTABLE;
 ----------
@@ -4085,7 +4085,7 @@ begin
 
   return query
    with
-	indata as ( select r.tbl, c.reltuples::bigint from pg_class c , (select unnest( tbls ) as tbl ) r	where c.oid = r.tbl ),
+	indata as ( select r.tbl, greatest( c.reltuples::bigint, 1) as reltuples from pg_class c , (select unnest( tbls ) as tbl ) r	where c.oid = r.tbl ),
 	iindata as ( select tbl, x.reltuples, x.reltuples::float/max(x.reltuples) over () as rweight, sum(x.reltuples) over () as total from indata x ),
     qkdata as
     (
