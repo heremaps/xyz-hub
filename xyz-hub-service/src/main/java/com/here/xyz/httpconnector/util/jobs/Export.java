45dcdd9b35588e9b33907a89b8d1fcba9289b7f3
Benjamin Rögner <benjamin.roegner@here.com>
2023-09-22 15:31:38 +0200

    Add hot-fix for race-conditions in job scheduler
    Signed-off-by: Benjamin Rögner <benjamin.roegner@here.com>

--- xyz-hub-service/src/main/java/com/here/xyz/httpconnector/util/jobs/Export.java
+++ xyz-hub-service/src/main/java/com/here/xyz/httpconnector/util/jobs/Export.java
@@ -58,6 +58,7 @@ import io.vertx.core.Future;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -71,6 +72,8 @@ public class Export extends Job<Export> {
     private static int VML_EXPORT_MIN_TARGET_LEVEL = 4;
     private static int VML_EXPORT_MAX_TARGET_LEVEL = 13;
     private static int VML_EXPORT_MAX_TILES_PER_FILE = 8192;
+    @JsonIgnore
+    private AtomicBoolean executing = new AtomicBoolean();
 
     @JsonView({Public.class})
     private Map<String,ExportObject> exportObjects;
@@ -783,6 +786,9 @@ public class Export extends Job<Export> {
 
     @Override
     public void execute() {
+        if (!executing.compareAndSet(false, true))
+            return;
+
         setExecutedAt(Core.currentTimeMillis() / 1000L);
 
         if (readParamPersistExport()) {
