0ae00bc798cdcdc32eb428fcfacfbe28cf4916fe
Maximilian Chrzan <maximilian.chrzan@here.com>
2023-10-20 12:39:42 +0200

    Add check if job is still in job queue before updating status.
    Signed-off-by: Maximilian Chrzan <maximilian.chrzan@here.com>

--- xyz-hub-service/src/main/java/com/here/xyz/httpconnector/util/scheduler/JobQueue.java
+++ xyz-hub-service/src/main/java/com/here/xyz/httpconnector/util/scheduler/JobQueue.java
@@ -20,10 +20,10 @@ package com.here.xyz.httpconnector.util.scheduler;
 
 import static com.here.xyz.httpconnector.util.jobs.Job.Status.failed;
 import static com.here.xyz.httpconnector.util.jobs.Job.Status.finalized;
+import static com.here.xyz.httpconnector.util.jobs.Job.Status.aborted;
 
 import com.here.xyz.httpconnector.CService;
 import com.here.xyz.httpconnector.config.JDBCClients;
-import com.here.xyz.httpconnector.util.jobs.Export;
 import com.here.xyz.httpconnector.util.jobs.Import;
 import com.here.xyz.httpconnector.util.jobs.Job;
 import com.here.xyz.httpconnector.util.web.HubWebClient;
@@ -143,7 +143,6 @@ public abstract class JobQueue implements Runnable {
     }
 
     public static Future<Job> setJobAborted(Job j) {
-        removeJob(j);
         return updateJobStatus(j, Job.Status.aborted);
     }
 
@@ -156,6 +155,11 @@ public abstract class JobQueue implements Runnable {
     }
 
     protected static Future<Job> updateJobStatus(Job job, Job.Status status, String errorDescription, String errorType) {
+        if(hasJob(job) == null) {
+            logger.warn("[{}] Job is already removed from queue, dont update status {}!", job.getId(), job.getStatus());
+            return Future.failedFuture("Job " + job.getId() + " is not in queue anymore!");
+        }
+
         if (status != null)
             job.setStatus(status);
         if (errorType != null)
@@ -164,7 +168,7 @@ public abstract class JobQueue implements Runnable {
             job.setErrorDescription(errorDescription);
 
         //All end-states
-        if (status == failed || status == finalized) {
+        if (status.equals(failed) || status.equals(finalized) || status.equals(aborted)) {
             //FIXME: Shouldn't that be done also for status == aborted? If yes, we can simply use status.isFinal()
             if (job instanceof Import) {
                 releaseReadOnlyLockFromSpace(job)
