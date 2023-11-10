6383d133e5c83236cbc9381a9ef667423c1c83f7
Benjamin Rögner <benjamin.roegner@here.com>
2023-11-10 09:28:10 +0100

    Fix for missing searchable properties in delta export
    Signed-off-by: Benjamin Rögner <benjamin.roegner@here.com>

--- xyz-hub-service/src/main/java/com/here/xyz/httpconnector/util/jobs/Export.java
+++ xyz-hub-service/src/main/java/com/here/xyz/httpconnector/util/jobs/Export.java
@@ -300,7 +300,9 @@ public class Export extends JDBCBasedJob<Export> {
 
         if (getEstimatedFeatureCount() > 1000000 //searchable limit without index
             && getPartitionKey() != null && !"id".equals(getPartitionKey())
-            && !searchableProperties.containsKey(getPartitionKey().replaceFirst("^(p|properties)\\." ,"")))
+            && searchableProperties != null
+            && !searchableProperties.containsKey(getPartitionKey().replaceFirst("^(p|properties)\\." ,""))
+            && !"tileid".equals(getPartitionKey()))
             throw new HttpException(BAD_REQUEST, "partitionKey [" + getPartitionKey() + "] is not a searchable property");
 
         return this;
