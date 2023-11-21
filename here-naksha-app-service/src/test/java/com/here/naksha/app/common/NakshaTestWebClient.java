bd1384a75b2991f54bc57419be09b26cf10d4150
Hiren Patel <hiren.patel@here.com>
2023-11-22 01:05:55 +0530

    Revert "Merge branch 'test-unit' into Naksha_maintenance"
    This reverts commit 12f800bb94a0a5fbd6b844cc8f5282e85968af20, reversing
    changes made to e2f52dc8b7510d641ba98379b2c5b549a9a5a887.

--- here-naksha-app-service/src/test/java/com/here/naksha/app/common/NakshaTestWebClient.java
+++ here-naksha-app-service/src/test/java/com/here/naksha/app/common/NakshaTestWebClient.java
@@ -43,7 +43,7 @@ public class NakshaTestWebClient {
   private static final Logger logger = LoggerFactory.getLogger(NakshaTestWebClient.class);
   private static final String NAKSHA_HTTP_URI = "http://localhost:8080/";
   private static final Duration CONNECT_TIMEOUT = Duration.ofSeconds(10);
-  private static final Duration SOCKET_TIMEOUT = Duration.ofSeconds(10);
+  private static final Duration SOCKET_TIMEOUT = Duration.ofSeconds(2);
 
   private final HttpClient httpClient;
 
