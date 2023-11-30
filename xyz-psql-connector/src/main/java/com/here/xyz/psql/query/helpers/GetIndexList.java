e6461b8191245924eccf43f953816c4cc41affb1
qGYdXbY2 <47661341+qGYdXbY2@users.noreply.github.com>
2023-11-30 17:43:21 +0100

    fix /search - error on non-indexed property
    Signed-off-by: qGYdXbY2 <47661341+qGYdXbY2@users.noreply.github.com>

--- xyz-psql-connector/src/main/java/com/here/xyz/psql/query/helpers/GetIndexList.java
+++ xyz-psql-connector/src/main/java/com/here/xyz/psql/query/helpers/GetIndexList.java
@@ -55,8 +55,11 @@ public class GetIndexList extends QueryRunner<String, List<String>> {
   @Override
   protected SQLQuery buildQuery(String tableName) throws SQLException, ErrorResponseException {
     return new SQLQuery("SELECT idx_available FROM " + ModifySpace.IDX_STATUS_TABLE_FQN
-        + " WHERE spaceid = #{table} AND count >= #{threshold}")
+        + " WHERE spaceid = #{table} "
+        + "  AND (select coalesce( (count->'value')::bigint, 0 ) from xyz_statistic_space(#{schema},#{table}, false)) >= #{threshold} "
+        )
         .withNamedParameter(TABLE, tableName)
+        .withNamedParameter(SCHEMA, getSchema())
         .withNamedParameter("threshold", BIG_SPACE_THRESHOLD);
   }
 
