d28cd22ea3111ec84637b2ed33f04af208e8bf5b
qGYdXbY2 <47661341+qGYdXbY2@users.noreply.github.com>
2023-12-11 15:35:35 +0100

    fix /search - error on non-indexed property (#1129)
    * fix /search - error on non-indexed property
    
    Signed-off-by: qGYdXbY2 <47661341+qGYdXbY2@users.noreply.github.com>
    
    * fx case idx_available = null on large table
    
    Signed-off-by: qGYdXbY2 <47661341+qGYdXbY2@users.noreply.github.com>
    
    * disable test - readByBoundingBoxWithQuadbinClusteringAndPropertySearch
    
    Signed-off-by: qGYdXbY2 <47661341+qGYdXbY2@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: qGYdXbY2 <47661341+qGYdXbY2@users.noreply.github.com>

--- xyz-psql-connector/src/main/java/com/here/xyz/psql/query/helpers/GetIndexList.java
+++ xyz-psql-connector/src/main/java/com/here/xyz/psql/query/helpers/GetIndexList.java
@@ -54,9 +54,12 @@ public class GetIndexList extends QueryRunner<String, List<String>> {
 
   @Override
   protected SQLQuery buildQuery(String tableName) throws SQLException, ErrorResponseException {
-    return new SQLQuery("SELECT idx_available FROM " + ModifySpace.IDX_STATUS_TABLE_FQN
-        + " WHERE spaceid = #{table} AND count >= #{threshold}")
+    return new SQLQuery("SELECT coalesce(idx_available,'[]'::jsonb) as idx_available FROM " + ModifySpace.IDX_STATUS_TABLE_FQN
+        + " WHERE spaceid = #{table} "
+        + "  AND (select coalesce( (count->'value')::bigint, 0 ) from xyz_statistic_space(#{schema},#{table}, false)) >= #{threshold} "
+        )
         .withNamedParameter(TABLE, tableName)
+        .withNamedParameter(SCHEMA, getSchema())
         .withNamedParameter("threshold", BIG_SPACE_THRESHOLD);
   }
 
