1778ad980bdb957a9f76d721440afb93fadc1901
Benjamin Rögner <benjamin.roegner@here.com>
2024-01-11 19:39:14 +0100

    Remove obsolete admin-events API
    - Also remove according FeatureTasks, test class and resource file
    
    Signed-off-by: Benjamin Rögner <benjamin.roegner@here.com>

--- xyz-hub-service/src/main/java/com/here/xyz/hub/rest/AdminApi.java
+++ xyz-hub-service/src/main/java/com/here/xyz/hub/rest/AdminApi.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017-2023 HERE Europe B.V.
+ * Copyright (C) 2017-2024 HERE Europe B.V.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,25 +20,11 @@
 package com.here.xyz.hub.rest;
 
 import static com.here.xyz.hub.rest.Api.HeaderValues.APPLICATION_JSON;
-import static com.here.xyz.hub.rest.ApiParam.Query.SKIP_CACHE;
 import static io.netty.handler.codec.http.HttpHeaderValues.TEXT_PLAIN;
-import static io.netty.handler.codec.http.HttpResponseStatus.BAD_REQUEST;
 import static io.netty.handler.codec.http.HttpResponseStatus.NO_CONTENT;
 import static io.netty.handler.codec.http.HttpResponseStatus.OK;
 import static io.vertx.core.http.HttpHeaders.CONTENT_TYPE;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.here.xyz.XyzSerializable;
-import com.here.xyz.events.Event;
-import com.here.xyz.events.GetFeaturesByBBoxEvent;
-import com.here.xyz.events.GetFeaturesByGeometryEvent;
-import com.here.xyz.events.GetFeaturesByIdEvent;
-import com.here.xyz.events.GetFeaturesByTileEvent;
-import com.here.xyz.events.GetStatisticsEvent;
-import com.here.xyz.events.IterateFeaturesEvent;
-import com.here.xyz.events.LoadFeaturesEvent;
-import com.here.xyz.events.ModifyFeaturesEvent;
-import com.here.xyz.events.SearchForFeaturesEvent;
 import com.here.xyz.hub.Service;
 import com.here.xyz.hub.auth.ActionMatrix;
 import com.here.xyz.hub.auth.Authorization;
@@ -48,14 +34,6 @@ import com.here.xyz.hub.auth.XyzHubAttributeMap;
 import com.here.xyz.hub.connectors.RemoteFunctionClient;
 import com.here.xyz.hub.connectors.statistics.StorageStatisticsProvider;
 import com.here.xyz.hub.rest.ApiParam.Query;
-import com.here.xyz.hub.task.FeatureTask;
-import com.here.xyz.hub.task.FeatureTask.GeometryQuery;
-import com.here.xyz.hub.task.FeatureTask.IdsQuery;
-import com.here.xyz.hub.task.FeatureTask.IterateQuery;
-import com.here.xyz.hub.task.FeatureTask.LoadFeaturesQuery;
-import com.here.xyz.hub.task.FeatureTask.SearchQuery;
-import com.here.xyz.hub.task.FeatureTask.TileQuery;
-import io.netty.handler.codec.http.HttpResponseStatus;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.http.HttpMethod;
@@ -72,7 +50,6 @@ public class AdminApi extends Api {
 
   public static final String MAIN_ADMIN_ENDPOINT = "/hub/admin/";
   public static final String ADMIN_MESSAGES_ENDPOINT = MAIN_ADMIN_ENDPOINT + "messages";
-  public static final String ADMIN_EVENTS_ENDPOINT = MAIN_ADMIN_ENDPOINT + "events";
   public static final String ADMIN_STORAGE_STATISTICS = MAIN_ADMIN_ENDPOINT + "statistics/spaces/storage";
   public static final String ADMIN_METRICS_ENDPOINT = MAIN_ADMIN_ENDPOINT + "metrics";
 
@@ -86,11 +63,6 @@ public class AdminApi extends Api {
         .handler(BodyHandler.create())
         .handler(auth)
         .handler(this::onMessage);
-
-    router.route(HttpMethod.POST, ADMIN_EVENTS_ENDPOINT)
-        .handler(BodyHandler.create())
-        .handler(auth)
-        .handler(this::onEvent);
 
     router.route(HttpMethod.GET, ADMIN_STORAGE_STATISTICS)
         .handler(auth)
@@ -166,57 +138,6 @@ public class AdminApi extends Api {
 
   private String _buildPrometheusResponsePart(String name, String description, double value) {
      return "# HELP " + name + " " + description + "\n# TYPE " + name + " gauge\n" + name + "{hpa=\"true\"} " + value + "\n";
-  }
-
-  /**
-   * Posts an event directly to the tasks handlers
-   */
-  private void onEvent(final RoutingContext context) {
-    final Marker marker = Context.getMarker(context);
-    final String body = context.body().asString();
-    final boolean skipCache = Query.getBoolean(context, SKIP_CACHE, false);
-
-    try {
-      final Event event = XyzSerializable.deserialize(context.body().asString());
-      logger.info("Event is " + event.getClass().getSimpleName());
-      if (event instanceof LoadFeaturesEvent) {
-        new LoadFeaturesQuery((LoadFeaturesEvent) event, context, ApiResponseType.FEATURE_COLLECTION, skipCache)
-            .execute(this::sendResponse, this::sendErrorResponse);
-      } else if (event instanceof GetFeaturesByIdEvent) {
-        new IdsQuery((GetFeaturesByIdEvent) event, context, ApiResponseType.FEATURE_COLLECTION, skipCache)
-            .execute(this::sendResponse, this::sendErrorResponse);
-      } else if (event instanceof GetFeaturesByTileEvent) {
-        new TileQuery((GetFeaturesByTileEvent) event, context, ApiResponseType.FEATURE_COLLECTION, skipCache)
-            .execute(this::sendResponse, this::sendErrorResponse);
-      } else if (event instanceof GetFeaturesByBBoxEvent) {
-        new FeatureTask.BBoxQuery((GetFeaturesByBBoxEvent) event, context, ApiResponseType.FEATURE_COLLECTION, skipCache)
-            .execute(this::sendResponse, this::sendErrorResponse);
-      } else if (event instanceof GetStatisticsEvent) {
-        new FeatureTask.GetStatistics((GetStatisticsEvent) event, context, ApiResponseType.STATISTICS_RESPONSE, skipCache)
-             .execute(this::sendResponse, this::sendErrorResponse);
-      } else if (event instanceof ModifyFeaturesEvent) {
-        new FeatureTask.ModifyFeaturesTask((ModifyFeaturesEvent) event, context, ApiResponseType.FEATURE_COLLECTION, skipCache)
-            .execute(this::sendResponse, this::sendErrorResponse);
-      } else if (event instanceof GetFeaturesByGeometryEvent) {
-          new GeometryQuery((GetFeaturesByGeometryEvent) event, context, ApiResponseType.FEATURE_COLLECTION, skipCache)
-              .execute(this::sendResponse, this::sendErrorResponse);
-      } else if (event instanceof IterateFeaturesEvent) {
-        new IterateQuery((IterateFeaturesEvent) event, context, ApiResponseType.FEATURE_COLLECTION, skipCache)
-            .execute(this::sendResponse, this::sendErrorResponse);
-      }  else if (event instanceof SearchForFeaturesEvent) {
-        new SearchQuery((SearchForFeaturesEvent) event, context, ApiResponseType.FEATURE_COLLECTION, skipCache)
-            .execute(this::sendResponse, this::sendErrorResponse);
-      } else {
-        logger.warn("Event cannot be handled: " + body);
-        sendErrorResponse(context, new HttpException(HttpResponseStatus.BAD_REQUEST, "Event cannot be handled"));
-      }
-    } catch (JsonProcessingException e) {
-      logger.warn(marker, "Error processing the event payload", e);
-      sendErrorResponse(context, new HttpException(BAD_REQUEST, "Error processing the event payload", e));
-    } catch (Exception e) {
-      logger.error(marker, "General error processing the event", e);
-      context.fail(e);
-    }
   }
 
   private static class AdminAuthorization extends Authorization {
