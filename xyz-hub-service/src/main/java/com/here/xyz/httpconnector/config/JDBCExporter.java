239cbf3da73ed46154190dd93ca40ba5c1828d4c
Ansari, Mujammil <mujammil.ansari@here.com>
2024-01-19 21:38:56 +0530

    Synchronize substitute method to avoid race conditions
    Signed-off-by: Ansari, Mujammil <mujammil.ansari@here.com>

--- xyz-hub-service/src/main/java/com/here/xyz/httpconnector/config/JDBCExporter.java
+++ xyz-hub-service/src/main/java/com/here/xyz/httpconnector/config/JDBCExporter.java
@@ -119,8 +119,7 @@ public class JDBCExporter extends JdbcBasedHandler {
           .withQueryFragment("jobId", job.getId())
           .withQueryFragment("targetVersioningEnabled", "" + targetVersioningEnabled)
           .withVariable("versionSequenceName", tableName + "_version_seq")
-          .withQueryFragment("contentQuery", buildCopyContentQuery(client, job, enableHashedSpaceId))
-          .substitute();
+          .withQueryFragment("contentQuery", buildCopyContentQuery(client, job, enableHashedSpaceId));
     }
 
   private static SQLQuery buildCopyContentQuery(JdbcClient client, Export job, boolean enableHashedSpaceId) throws SQLException {
