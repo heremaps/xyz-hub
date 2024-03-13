25db67dcd3f8dd2f85bb6e8e2d6a41c9f3480e69
mchrza <maximilian.chrzan@here.com>
2024-03-13 12:25:20 +0100

    Add lambda timeout and set region to us-east-1
    Signed-off-by: Maximilian Chrzan <maximilian.chrzan@here.com>
    Signed-off-by: mchrza <maximilian.chrzan@here.com>

--- xyz-jobs/xyz-job-steps/src/main/bash/deployLocalLambda.sh
+++ xyz-jobs/xyz-job-steps/src/main/bash/deployLocalLambda.sh
@@ -11,15 +11,16 @@ zip -r xyz-job-steps.zip lib
 
 #Delete a potentially existing old local Lambda Function with the same name
 aws --endpoint http://localhost:4566 lambda delete-function \
-  --region eu-west-1 \
+  --region us-east-1 \
   --function-name job-step \
   > /dev/null 2>&1
 
 aws --endpoint http://localhost:4566 lambda create-function \
-  --region eu-west-1 \
+  --timeout 30 \
+  --region us-east-1 \
   --function-name job-step \
   --runtime java17 \
   --zip-file fileb://xyz-job-steps.zip \
   --handler 'com.here.xyz.jobs.steps.execution.LambdaBasedStep$LambdaBasedStepExecutor::handleRequest' \
   --role arn:aws:iam::000000000000:role/lambda-role \
-  --environment 'Variables={HUB_ENDPOINT=http://host.docker.internal:8080/hub,ECPS_PHRASE=local,JOBS_S3_BUCKET=test-bucket,LOCALSTACK_ENDPOINT=http://localstack:4566,LOCAL_DB_HOST_OVERRIDE=postgres}'
+  --environment 'Variables={HUB_ENDPOINT=http://host.docker.internal:8080/hub,ECPS_PHRASE=local,JOBS_S3_BUCKET=test-bucket,JOBS_REGION=us-east-1,LOCALSTACK_ENDPOINT=http://localstack:4566,LOCAL_DB_HOST_OVERRIDE=postgres}'
