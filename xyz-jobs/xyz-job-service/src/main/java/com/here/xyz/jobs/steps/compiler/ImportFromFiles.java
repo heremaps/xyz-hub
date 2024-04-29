c69147dbf5c4b24e6fa2b519c06e891c0b08786b
Benjamin Rögner <benjamin.roegner@here.com>
2024-04-29 23:42:01 +0200

    Use correct legacy CSV formats for Imports in ImportFromFiles compilation interceptor
    Signed-off-by: Benjamin Rögner <benjamin.roegner@here.com>

--- xyz-jobs/xyz-job-service/src/main/java/com/here/xyz/jobs/steps/compiler/ImportFromFiles.java
+++ xyz-jobs/xyz-job-service/src/main/java/com/here/xyz/jobs/steps/compiler/ImportFromFiles.java
@@ -19,25 +19,34 @@
 
 package com.here.xyz.jobs.steps.compiler;
 
+import static com.here.xyz.jobs.steps.impl.imp.ImportFilesToSpace.Format.CSV_GEOJSON;
+import static com.here.xyz.jobs.steps.impl.imp.ImportFilesToSpace.Format.CSV_JSONWKB;
+import static com.here.xyz.jobs.steps.impl.imp.ImportFilesToSpace.Format.GEOJSON;
 import static com.here.xyz.util.db.pg.XyzSpaceTableHelper.Index.VIZ;
 
 import com.google.common.collect.Lists;
 import com.here.xyz.jobs.Job;
 import com.here.xyz.jobs.datasets.DatasetDescription;
 import com.here.xyz.jobs.datasets.Files;
+import com.here.xyz.jobs.datasets.files.Csv;
+import com.here.xyz.jobs.datasets.files.FileFormat;
+import com.here.xyz.jobs.datasets.files.GeoJson;
 import com.here.xyz.jobs.steps.CompilationStepGraph;
+import com.here.xyz.jobs.steps.JobCompiler.CompilationError;
 import com.here.xyz.jobs.steps.StepExecution;
 import com.here.xyz.jobs.steps.StepGraph;
 import com.here.xyz.jobs.steps.impl.AnalyzeSpaceTable;
 import com.here.xyz.jobs.steps.impl.CreateIndex;
 import com.here.xyz.jobs.steps.impl.DropIndexes;
 import com.here.xyz.jobs.steps.impl.MarkForMaintenance;
+import com.here.xyz.jobs.steps.impl.imp.ImportFilesToSpace;
+import com.here.xyz.jobs.steps.impl.imp.ImportFilesToSpace.Format;
 import com.here.xyz.util.db.pg.XyzSpaceTableHelper.Index;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class ImportFromFiles implements JobCompilationInterceptor{
+public class ImportFromFiles implements JobCompilationInterceptor {
 
   @Override
   public boolean chooseMe(Job job) {
@@ -52,14 +61,25 @@ public class ImportFromFiles implements JobCompilationInterceptor{
     //Split the work in two parallel tasks for now
     List<List<Index>> indexTasks = Lists.partition(indices, indices.size() / 2);
 
+    final FileFormat sourceFormat = ((Files) job.getSource()).getInputSettings().getFormat();
+    Format importStepFormat;
+    if (sourceFormat instanceof GeoJson)
+      importStepFormat = GEOJSON;
+    else if (sourceFormat instanceof Csv csvFormat)
+      importStepFormat = csvFormat.isGeometryAsExtraWkbColumn() ? CSV_JSONWKB : CSV_GEOJSON;
+    else
+      throw new CompilationError("Unsupported import file format: " + sourceFormat.getClass().getSimpleName());
+
     return new CompilationStepGraph(job.getId())
-        .addExecution(new DropIndexes().withSpaceId(spaceId)) // Drop all existing indices
-        .addExecution(new com.here.xyz.jobs.steps.impl.imp.ImportFilesToSpace().withSpaceId(spaceId)) // Perform import
+        .addExecution(new DropIndexes().withSpaceId(spaceId)) //Drop all existing indices
+        .addExecution(new ImportFilesToSpace() //Perform import
+            .withSpaceId(spaceId)
+            .withFormat(importStepFormat))
         //NOTE: Create *all* indices in parallel, make sure to (at least) keep the viz-index sequential #postgres-issue-with-partitions
         .addExecution(new CompilationStepGraph(job.getId()) //Create all the base indices semi-parallel
             .addExecution(new CompilationStepGraph(job.getId()).withExecutions(toSequentialSteps(spaceId, indexTasks.get(0))))
             .addExecution(new CompilationStepGraph(job.getId()).withExecutions(toSequentialSteps(spaceId, indexTasks.get(1))))
-            .withParallel(true))
+            .withParallel(false))
         .addExecution(new CreateIndex().withIndex(VIZ).withSpaceId(spaceId))
         .addExecution(new AnalyzeSpaceTable().withSpaceId(spaceId))
         .addExecution(new MarkForMaintenance().withSpaceId(spaceId));
