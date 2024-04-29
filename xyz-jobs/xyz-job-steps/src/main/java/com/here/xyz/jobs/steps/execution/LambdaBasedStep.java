7c99a6f30cb9cd6720afbcc6ab16db40c52ce77a
Benjamin Rögner <benjamin.roegner@here.com>
2024-04-29 14:10:24 +0200

    Add missing step.cancel() call when the step receives a FAILURE_CALLBACK request
    Signed-off-by: Benjamin Rögner <benjamin.roegner@here.com>

--- xyz-jobs/xyz-job-steps/src/main/java/com/here/xyz/jobs/steps/execution/LambdaBasedStep.java
+++ xyz-jobs/xyz-job-steps/src/main/java/com/here/xyz/jobs/steps/execution/LambdaBasedStep.java
@@ -455,7 +455,8 @@ public abstract class LambdaBasedStep<T extends LambdaBasedStep> extends Step<T>
             logger.info("Reported async success for step {} successfully.", request.getStep().getGlobalStepId());
           }
           case FAILURE_CALLBACK -> {
-            logger.info("Reporting async failure for step {} ...", request.getStep().getGlobalStepId());
+            logger.info("Cancelling and reporting async failure for step {} ...", request.getStep().getGlobalStepId());
+            request.getStep().cancel();
             //NOTE: Assume that the error information has been injected into the status object by the callback caller already
             request.getStep().reportFailure(null, false, true);
             logger.info("Reported async failure for step {} failure successfully.", request.getStep().getGlobalStepId());
