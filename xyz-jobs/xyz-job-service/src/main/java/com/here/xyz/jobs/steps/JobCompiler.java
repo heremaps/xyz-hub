09a3c6ca9e92ca0d23676e86016991c3a5a2f16f
mchrza <maximilian.chrzan@here.com>
2024-06-07 10:35:18 +0200

    Add CopySpaceToSpace Interceptor and extend compiler accordingly.
    Signed-off-by: Maximilian Chrzan <maximilian.chrzan@here.com>
    Signed-off-by: mchrza <maximilian.chrzan@here.com>

--- xyz-jobs/xyz-job-service/src/main/java/com/here/xyz/jobs/steps/JobCompiler.java
+++ xyz-jobs/xyz-job-service/src/main/java/com/here/xyz/jobs/steps/JobCompiler.java
@@ -20,6 +20,7 @@
 package com.here.xyz.jobs.steps;
 
 import com.here.xyz.jobs.Job;
+import com.here.xyz.jobs.steps.compiler.CopySpaceToSpace;
 import com.here.xyz.jobs.steps.compiler.ImportFromFiles;
 import com.here.xyz.jobs.steps.compiler.JobCompilationInterceptor;
 import com.here.xyz.util.Async;
@@ -38,6 +39,7 @@ public class JobCompiler {
 
   static {
     registerCompilationInterceptor(ImportFromFiles.class);
+    registerCompilationInterceptor(CopySpaceToSpace.class);
   }
 
   public Future<StepGraph> compile(Job job) {
