79f033282c00d1ba0490e7aa4872be02217b9f50
Hiren Patel <80465571+hirenkp2000@users.noreply.github.com>
2024-06-12 19:34:52 +0530

    Fixed propagation of error result in case of Patch request (#294)
    * Fixed propagation of error result in case of Patch request
    
    * Cloud-config Add locationtech.jts to whitelistClasses.
    
    * corrected log message
    
    ---------
    
    Co-authored-by: lakde <rohit.lakde@here.com>

--- here-naksha-app-service/src/main/java/com/here/naksha/app/service/http/tasks/WriteFeatureApiTask.java
+++ here-naksha-app-service/src/main/java/com/here/naksha/app/service/http/tasks/WriteFeatureApiTask.java
@@ -357,9 +357,10 @@ public class WriteFeatureApiTask<T extends XyzResponse> extends AbstractApiTask<
               spaceId, featuresFromRequest, responseType, addTags, removeTags, retry + 1);
         } catch (NoCursor e) {
           return returnError(
-              XyzError.EXCEPTION,
-              "Unexpected response when trying to persist patched features.",
-              "No cursor when analyzing error result, while attempting to write patched features into storage.");
+              er.reason,
+              er.message,
+              "No cursor when analyzing error result, while attempting to write patched features into storage. "
+                  + er);
         }
       } else {
         if (responseType.equals(HttpResponseType.FEATURE)) {
