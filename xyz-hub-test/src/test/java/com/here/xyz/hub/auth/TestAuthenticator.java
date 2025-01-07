8dac0cbd9203ba47e6c9edc24f76ab2db6570ff2
Dimitar Goshev <dimitar.goshev@here.com>
2025-01-07 11:53:02 +0100

    Skip authorization when the service is started with authorization turned off
    Signed-off-by: Dimitar Goshev <dimitar.goshev@here.com>

--- xyz-hub-test/src/test/java/com/here/xyz/hub/auth/TestAuthenticator.java
+++ xyz-hub-test/src/test/java/com/here/xyz/hub/auth/TestAuthenticator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017-2021 HERE Europe B.V.
+ * Copyright (C) 2017-2025 HERE Europe B.V.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -111,7 +111,7 @@ public class TestAuthenticator {
 
     AuthProfile() {
       String resourceFilename = "/auth/" + name() + ".json";
-      this.payload = JwtGenerator.readTokenPayload(resourceFilename);
+      this.payload = JwtGenerator.readTokenPayload(resourceFilename, false);
       jwt_string = JwtGenerator.generateToken(this.payload);
     }
   }
