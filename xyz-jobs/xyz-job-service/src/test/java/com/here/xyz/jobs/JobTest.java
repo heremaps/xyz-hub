0b726d24f71d29ba7b88c0de7a132633d6f26e70
Benjamin Rögner <benjamin.roegner@here.com>
2025-02-07 15:28:49 +0100

    Implement test for two parallel running exports
    Signed-off-by: Benjamin Rögner <benjamin.roegner@here.com>

--- xyz-jobs/xyz-job-service/src/test/java/com/here/xyz/jobs/JobTest.java
+++ xyz-jobs/xyz-job-service/src/test/java/com/here/xyz/jobs/JobTest.java
@@ -1,67 +1,74 @@
 package com.here.xyz.jobs;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import com.here.xyz.jobs.util.test.JobTestBase;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 
 public class JobTest extends JobTestBase {
 
-    @AfterEach
-    public void tearDown() throws IOException, InterruptedException {
-        cleanResources();
-    }
+  @AfterEach
+  public void tearDown() throws IOException, InterruptedException {
+    cleanResources();
+  }
 
-    protected void checkSucceededJob(Job exportJob, int expectedFeatureCount) throws IOException, InterruptedException {
-        checkRuntimeStatus(exportJob.getId());
-        checkOutputs(exportJob, expectedFeatureCount);
-    }
+  protected void checkSucceededJob(Job exportJob, int expectedFeatureCount) throws IOException, InterruptedException {
+    checkRuntimeStatus(exportJob.getId());
+    checkOutputs(exportJob, expectedFeatureCount);
+  }
 
-    protected void checkJobReusage(Job reusedJob, Job exportJob, int expectedFeatureCount, boolean expectMatch) throws IOException, InterruptedException {
-        checkRuntimeStatus(exportJob.getId());
-        checkOutputs(reusedJob, exportJob, expectedFeatureCount, expectMatch);
-    }
+  private void checkRuntimeStatus(String jobId) throws IOException, InterruptedException {
+    RuntimeStatus status = getJobStatus(jobId);
+    assertEquals(RuntimeInfo.State.SUCCEEDED, status.getState());
+    assertEquals(status.getOverallStepCount(), status.getSucceededSteps());
+  }
 
-    private void checkRuntimeStatus(String jobId) throws IOException, InterruptedException {
-        RuntimeStatus status = getJobStatus(jobId);
-        Assertions.assertEquals(RuntimeInfo.State.SUCCEEDED, status.getState());
-        Assertions.assertEquals(status.getOverallStepCount(), status.getSucceededSteps());
-    }
+  private void checkOutputs(Job exportJob, int expectedFeatureCount) throws IOException, InterruptedException {
+    checkOutputs(null, exportJob, expectedFeatureCount, false);
+  }
 
-    private void checkOutputs(Job exportJob, int expectedFeatureCount) throws IOException, InterruptedException {
-        checkOutputs(null, exportJob, expectedFeatureCount, false);
-    }
+  private void checkOutputs(Job firstJob, Job secondJob, int expectedFeatureCount, boolean expectMatch)
+      throws IOException, InterruptedException {
+    boolean foundStatistics = false;
+    boolean foundUrls = false;
 
-    private void checkOutputs(Job firstJob, Job secondJob, int expectedFeatureCount, boolean expectMatch) throws IOException, InterruptedException {
-        boolean foundStatistics = false;
-        boolean foundUrls = false;
-
-        List<Map> jobOutputs = getJobOutputs(secondJob.getId());
-        for (Map jobOutput : jobOutputs) {
-            if(jobOutput.get("type").equals("FeatureStatistics")){ //TODO: Use the FeatureStatistics model here instead
-                foundStatistics = true;
-                Assertions.assertEquals(expectedFeatureCount, jobOutput.get("featureCount"));
-            }else if(jobOutput.get("type").equals("DownloadUrl")){
-                foundUrls = true;
-                if(expectMatch) {
-                    //all links should point to firstJob (reused) Job
-                    Assertions.assertTrue(((String) jobOutput.get("url")).contains(firstJob.getId()));
-                    //no link should have a reference to the new job (secondJob)
-                    Assertions.assertFalse(((String) jobOutput.get("url")).contains(secondJob.getId()));
-                }else{
-                    //all links should point to secondJob as reference
-                    Assertions.assertTrue(((String) jobOutput.get("url")).contains(secondJob.getId()));
-                    if(firstJob != null){
-                        //no link should have a reference to the firstJob (noMatch)
-                        Assertions.assertFalse(((String) jobOutput.get("url")).contains(firstJob.getId()));
-                    }
-                }
-            }
+    List<Map> jobOutputs = getJobOutputs(secondJob.getId());
+    for (Map jobOutput : jobOutputs) {
+      if (jobOutput.get("type").equals("FeatureStatistics")) {
+        foundStatistics = true;
+        assertEquals(expectedFeatureCount, jobOutput.get("featureCount"));
+      }
+      else if (jobOutput.get("type").equals("DownloadUrl")) {
+        foundUrls = true;
+        if (expectMatch) {
+          //all links should point to firstJob (reused) Job
+          assertTrue(((String) jobOutput.get("url")).contains(firstJob.getId()));
+          //no link should have a reference to the new job (secondJob)
+          assertFalse(((String) jobOutput.get("url")).contains(secondJob.getId()));
         }
-
-        Assertions.assertTrue(foundStatistics);
-        Assertions.assertTrue(foundUrls);
+        else {
+          //all links should point to secondJob as reference
+          assertTrue(((String) jobOutput.get("url")).contains(secondJob.getId()));
+          if (firstJob != null) {
+            //no link should have a reference to the firstJob (noMatch)
+            assertFalse(((String) jobOutput.get("url")).contains(firstJob.getId()));
+          }
+        }
+      }
     }
+
+    assertTrue(foundStatistics);
+    assertTrue(foundUrls);
+  }
+
+  protected void checkJobReusage(Job reusedJob, Job exportJob, int expectedFeatureCount, boolean expectMatch)
+      throws IOException, InterruptedException {
+    checkRuntimeStatus(exportJob.getId());
+    checkOutputs(reusedJob, exportJob, expectedFeatureCount, expectMatch);
+  }
 }
