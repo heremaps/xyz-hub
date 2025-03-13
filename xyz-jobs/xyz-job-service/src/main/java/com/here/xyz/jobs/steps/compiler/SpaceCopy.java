258a60eb2b1006208d0ee3978bae20980eda02fa
qGYdXbY2 <47661341+qGYdXbY2@users.noreply.github.com>
2025-03-13 12:22:34 +0100

    imlcopy: add source.ref n..n testcase (#1479)
    * imlcopy: add source.ref n..n testcase
    
    Signed-off-by: qGYdXbY2 <47661341+qGYdXbY2@users.noreply.github.com>
    
    * reverted and moved logic to iml
    
    Signed-off-by: qGYdXbY2 <47661341+qGYdXbY2@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: qGYdXbY2 <47661341+qGYdXbY2@users.noreply.github.com>

--- xyz-jobs/xyz-job-service/src/main/java/com/here/xyz/jobs/steps/compiler/SpaceCopy.java
+++ xyz-jobs/xyz-job-service/src/main/java/com/here/xyz/jobs/steps/compiler/SpaceCopy.java
@@ -34,7 +34,6 @@ import com.here.xyz.jobs.steps.impl.transport.CopySpace;
 import com.here.xyz.jobs.steps.impl.transport.CopySpacePost;
 import com.here.xyz.jobs.steps.impl.transport.CopySpacePre;
 import com.here.xyz.models.hub.Ref;
-import com.here.xyz.models.hub.Ref.InvalidRef;
 import com.here.xyz.responses.StatisticsResponse;
 import com.here.xyz.util.web.HubWebClient;
 import com.here.xyz.util.web.XyzWebClient.WebClientException;
@@ -42,11 +41,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class SpaceCopy implements JobCompilationInterceptor {
-  private static final Logger logger = LogManager.getLogger();
 
   public static Set<Class> forbiddenTargetTypes = new HashSet<>();
 
@@ -105,6 +101,9 @@ public class SpaceCopy implements JobCompilationInterceptor {
       sourceStatistics = hubWebClient().loadSpaceStatistics(sourceSpaceId, sourceContext);
       targetStatistics = hubWebClient().loadSpaceStatistics(targetSpaceId, targetContext);
 
+      Ref versionRef = source.getVersionRef(),
+          resolvedVersionRef = hubWebClient().resolveRef(sourceSpaceId, sourceContext, versionRef);
+
       CopySpacePre preCopySpace = new CopySpacePre().withSpaceId(targetSpaceId).withJobId(jobId);
 
       CompilationStepGraph startGraph = new CompilationStepGraph();
@@ -118,18 +117,6 @@ public class SpaceCopy implements JobCompilationInterceptor {
       CompilationStepGraph copyGraph = new CompilationStepGraph();
 
       for (int threadId = 0; threadId < threadCount; threadId++) {
-        Ref versionRef = source.getVersionRef();
-
-        Ref resolvedVersionRef = null;
-        try {
-          //TODO: Move resolving of version ref into step's prepare method! (See: ExportToFiles compiler)
-          resolvedVersionRef = hubWebClient().resolveRef(sourceSpaceId, sourceContext, versionRef);
-        } catch (WebClientException | InvalidRef e) {
-          logger.error("Error resolving versionRef " + versionRef + " - spaceId: " + sourceSpaceId , e);
-        }
-
-        if (resolvedVersionRef == null)
-         continue;
 
         CopySpace copySpaceStep = new CopySpace()
             .withSpaceId(sourceSpaceId)
