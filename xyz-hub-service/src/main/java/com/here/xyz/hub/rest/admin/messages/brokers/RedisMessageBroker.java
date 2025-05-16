f62f5232704c7d8bbdd65f5bc573fa0f486d56a9
mchrza <maximilian.chrzan@here.com>
2025-05-16 13:01:45 +0200

    DS-1306: disable HostnameVerification
    Signed-off-by: Maximilian Chrzan <maximilian.chrzan@here.com>
    Signed-off-by: mchrza <maximilian.chrzan@here.com>

--- xyz-hub-service/src/main/java/com/here/xyz/hub/rest/admin/messages/brokers/RedisMessageBroker.java
+++ xyz-hub-service/src/main/java/com/here/xyz/hub/rest/admin/messages/brokers/RedisMessageBroker.java
@@ -62,8 +62,8 @@ public class RedisMessageBroker implements MessageBroker {
     RedisOptions config = new RedisOptions()
         .setConnectionString(Service.configuration.getRedisUri())
         .setNetClientOptions(new NetClientOptions()
-            .setSsl(true)
-            .setHostnameVerificationAlgorithm("HTTPS") //required for hostname check
+            .setHostnameVerificationAlgorithm("") //TODO: temp disable hostname verification
+            .setTrustAll(true)
             .setTcpKeepAlive(true)
             .setIdleTimeout(withIdleTimeout ? CONNECTION_KEEP_ALIVE : 0)
             .setConnectTimeout(2000));
