c4274e60c058539b54808d5396241a6f0e9a7966
Ansari, Mujammil <mujammil.ansari@here.com>
2025-05-22 18:47:40 +0530

    Add Datasets to allow support for multiple dataset descriptions in source/target
    Signed-off-by: Ansari, Mujammil <mujammil.ansari@here.com>

---
+++ xyz-jobs/xyz-job-service/src/main/java/com/here/xyz/jobs/datasets/Datasets.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2017-2024 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package com.here.xyz.jobs.datasets;
+
+import java.util.List;
+import java.util.Map.Entry;
+
+public class Datasets extends DatasetDescription {
+
+  private java.util.Map<String, DatasetDescription> datasets;
+
+  public java.util.Map<String, DatasetDescription> getDatasets() {
+    return datasets;
+  }
+
+  public void setDatasets(java.util.Map<String, DatasetDescription> datasets) {
+    this.datasets = datasets;
+  }
+
+  public Datasets withDatasets(java.util.Map<String, DatasetDescription> datasets) {
+    setDatasets(datasets);
+    return this;
+  }
+
+  @Override
+  public String getKey() {
+    return datasets.values()
+            .stream()
+            .filter(description -> description.getKey() != null)
+            .map(DatasetDescription::getKey)
+            .findFirst()
+            .orElse(null);
+  }
+
+  public List<Entry<String, DatasetDescription>> getDatasetsByType(Class<? extends DatasetDescription> clazz) {
+    return datasets.entrySet().stream()
+            .filter(entry -> entry.getValue().getClass().isAssignableFrom(clazz))
+            .toList();
+  }
+}
