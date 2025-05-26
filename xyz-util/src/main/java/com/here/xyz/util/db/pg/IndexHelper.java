7f2638d17a6e86ca8abdc8fa483b95d807795004
Benjamin Rögner <benjamin.roegner@here.com>
2025-05-26 11:41:51 +0200

    DS-1064: Minor code improvements in space index creations
    Signed-off-by: Benjamin Rögner <benjamin.roegner@here.com>

--- xyz-util/src/main/java/com/here/xyz/util/db/pg/IndexHelper.java
+++ xyz-util/src/main/java/com/here/xyz/util/db/pg/IndexHelper.java
@@ -50,50 +50,49 @@ public class IndexHelper {
 
   private static SQLQuery buildCreateIndexQuery(String schema, String table, List<String> columnNamesOrExpressions, String method,
       String indexName, String predicate) {
-      return new SQLQuery("CREATE INDEX ${{queryComment}} IF NOT EXISTS ${indexName} ON ${schema}.${table} USING " + method
-          + " (" + String.join(", ", columnNamesOrExpressions) + ") ${{predicate}}")
-          .withVariable("schema", schema)
-          .withVariable("table", table)
-          .withVariable("indexName", indexName)
-          .withQueryFragment("predicate", predicate != null ? "WHERE " + predicate : "")
-          .withQueryFragment("queryComment", "");
+    return new SQLQuery("CREATE INDEX ${{queryComment}} IF NOT EXISTS ${indexName} ON ${schema}.${table} USING " + method
+        + " (" + String.join(", ", columnNamesOrExpressions) + ") ${{predicate}}")
+        .withVariable("schema", schema)
+        .withVariable("table", table)
+        .withVariable("indexName", indexName)
+        .withQueryFragment("predicate", predicate != null ? "WHERE " + predicate : "")
+        .withQueryFragment("queryComment", "");
   }
 
   public static SQLQuery buildDropIndexQuery(String schema, String indexName) {
     return new SQLQuery("DROP INDEX ${{queryComment}} IF EXISTS ${schema}.${indexName} CASCADE")
-            .withVariable("schema", schema)
-            .withVariable("indexName", indexName)
-            .withQueryFragment("queryComment", "");
+        .withVariable("schema", schema)
+        .withVariable("indexName", indexName)
+        .withQueryFragment("queryComment", "");
   }
 
-  //TODO: move xyz_index_creation_on_property_object, xyz_index_name_for_property, xyz_property_datatype functions from
-  // ext to common.sql ?
-  public static SQLQuery buildAsyncOnDemandIndexQuery(String schema, String table, String propertyName){
+  //TODO: move xyz_index_creation_on_property_object, xyz_index_name_for_property, xyz_property_datatype functions from ext to common.sql ?
+  public static SQLQuery buildAsyncOnDemandIndexQuery(String schema, String table, String propertyName) {
     return new SQLQuery("""
-            PERFORM xyz_index_creation_on_property_object(
-                #{schema_name},
-                #{table_name},
-                #{property_name},
-                xyz_index_name_for_property(#{table_name}, #{property_name}, #{idx_type}),
-                xyz_property_datatype(#{schema_name}, #{table_name}, #{property_name}, #{table_sample_cnt}),
-                #{idx_type}
-              )
-            """)
-            .withNamedParameter("schema_name", schema)
-            .withNamedParameter("table_name", table)
-            .withNamedParameter("property_name", propertyName)
-            .withNamedParameter("table_sample_cnt", 5000)
-            .withNamedParameter("idx_type", "m");
+        PERFORM xyz_index_creation_on_property_object(
+            #{schema_name},
+            #{table_name},
+            #{property_name},
+            xyz_index_name_for_property(#{table_name}, #{property_name}, #{idx_type}),
+            xyz_property_datatype(#{schema_name}, #{table_name}, #{property_name}, #{table_sample_cnt}),
+            #{idx_type}
+          )
+        """)
+        .withNamedParameter("schema_name", schema)
+        .withNamedParameter("table_name", table)
+        .withNamedParameter("property_name", propertyName)
+        .withNamedParameter("table_sample_cnt", 5000)
+        .withNamedParameter("idx_type", "m");
   }
 
   public static SQLQuery checkIndexType(String schema, String table, String propertyName, int tableSampleCnt) {
     return new SQLQuery("""
-            SELECT * FROM xyz_index_creation_on_property_object( #{schema_name}, #{table_name},
-                #{property_name}, #{table_sample_cnt} )
-            """)
-            .withNamedParameter("schema_name", schema)
-            .withNamedParameter("table_name", table)
-            .withNamedParameter("property_name", propertyName)
-            .withNamedParameter("table_sample_cnt", tableSampleCnt);
+        SELECT * FROM xyz_index_creation_on_property_object( #{schema_name}, #{table_name},
+            #{property_name}, #{table_sample_cnt} )
+        """)
+        .withNamedParameter("schema_name", schema)
+        .withNamedParameter("table_name", table)
+        .withNamedParameter("property_name", propertyName)
+        .withNamedParameter("table_sample_cnt", tableSampleCnt);
   }
 }
