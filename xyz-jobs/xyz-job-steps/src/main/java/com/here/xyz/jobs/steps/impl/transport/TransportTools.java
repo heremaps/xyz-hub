2b3dca86853fcd9e8a4d96973ce6d614746add19
Benjamin Rögner <benjamin.roegner@here.com>
2025-06-13 11:54:13 +0200

    DS-1064: Move import related parts TransportTools -> ImportFilesToSpace
    Signed-off-by: Benjamin Rögner <benjamin.roegner@here.com>

--- xyz-jobs/xyz-job-steps/src/main/java/com/here/xyz/jobs/steps/impl/transport/TransportTools.java
+++ xyz-jobs/xyz-job-steps/src/main/java/com/here/xyz/jobs/steps/impl/transport/TransportTools.java
@@ -19,30 +19,20 @@
 
 package com.here.xyz.jobs.steps.impl.transport;
 
-import com.here.xyz.jobs.steps.S3DataFile;
 import com.here.xyz.jobs.steps.Step;
 import com.here.xyz.jobs.steps.impl.SpaceBasedStep;
-import com.here.xyz.jobs.steps.inputs.UploadUrl;
-import com.here.xyz.jobs.steps.outputs.DownloadUrl;
-import com.here.xyz.jobs.util.S3Client;
 import com.here.xyz.util.db.SQLQuery;
-import io.vertx.core.json.JsonObject;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+//TODO: Move commonly used parts into the common parent class instead (once refactoring of import step was done)
+@Deprecated
 public class TransportTools {
   private static final Logger logger = LogManager.getLogger();
 
   private static final String JOB_DATA_PREFIX = "job_data_";
-  private static final String TRIGGER_TABLE_SUFFIX = "_trigger_tbl";
-
-  public static String getTemporaryTriggerTableName(String stepId) {
-    return getTemporaryJobTableName(stepId) + TRIGGER_TABLE_SUFFIX;
-  }
 
   public static String getTemporaryJobTableName(String stepId) {
     return JOB_DATA_PREFIX + stepId;
@@ -52,92 +42,6 @@ public class TransportTools {
     return new SQLQuery("DROP TABLE IF EXISTS ${schema}.${table};")
         .withVariable("table", tableName)
         .withVariable("schema", schema);
-  }
-
-  protected static SQLQuery buildTemporaryJobTableCreateStatement(String schema, Step step) {
-    return new SQLQuery("""
-        CREATE TABLE IF NOT EXISTS ${schema}.${table}
-               (
-                    s3_bucket text NOT NULL,
-                    s3_path text NOT NULL,
-                    s3_region text NOT NULL,
-                    state text NOT NULL, --jobtype
-                    execution_count int DEFAULT 0, --amount of retries
-                    data jsonb COMPRESSION lz4, --statistic data
-                    i SERIAL,
-                    CONSTRAINT ${primaryKey} PRIMARY KEY (s3_path)
-               );
-        """)
-        .withVariable("table", getTemporaryJobTableName(step.getId()))
-        .withVariable("schema", schema)
-        .withVariable("primaryKey", getTemporaryJobTableName(step.getId()) + "_primKey");
-  }
-
-  protected static String bucketRegion(String bucketName) {
-    return S3Client.getInstance(bucketName).region();
-  }
-
-  protected static List<SQLQuery> buildTemporaryJobTableInsertStatements(String schema, List<S3DataFile> fileList, Step step) {
-    List<SQLQuery> queryList = new ArrayList<>();
-    for (S3DataFile input : fileList) {
-      if (input instanceof UploadUrl || input instanceof DownloadUrl) {
-        JsonObject data = new JsonObject()
-            .put("compressed", input.isCompressed());
-
-        if (input instanceof UploadUrl)
-          data.put("filesize", input.getByteSize());
-
-        queryList.add(
-            new SQLQuery("""                
-                    INSERT INTO  ${schema}.${table} (s3_bucket, s3_path, s3_region, state, data)
-                        VALUES (#{bucketName}, #{s3Key}, #{bucketRegion}, #{state}, #{data}::jsonb)
-                        ON CONFLICT (s3_path) DO NOTHING;
-                """) //TODO: Why would we ever have a conflict here? Why to fill the table again on resume()?
-                .withVariable("schema", schema)
-                .withVariable("table", getTemporaryJobTableName(step.getId()))
-                .withNamedParameter("s3Key", input.getS3Key())
-                .withNamedParameter("bucketName", input.getS3Bucket())
-                .withNamedParameter("bucketRegion", bucketRegion(input.getS3Bucket()))
-                .withNamedParameter("state", "SUBMITTED")
-                .withNamedParameter("data", data.toString())
-        );
-      }
-    }
-    //Add final entry
-    queryList.add(
-        new SQLQuery("""                
-                INSERT INTO  ${schema}.${table} (s3_bucket, s3_path, s3_region, state, data)
-                    VALUES (#{bucketName}, #{s3Key}, #{bucketRegion}, #{state}, #{data}::jsonb)
-                    ON CONFLICT (s3_path) DO NOTHING;
-            """) //TODO: Why would we ever have a conflict here? Why to fill the table again on resume()?
-            .withVariable("schema", schema)
-            .withVariable("table", getTemporaryJobTableName(step.getId()))
-            .withNamedParameter("s3Key", "SUCCESS_MARKER")
-            .withNamedParameter("bucketName", "SUCCESS_MARKER")
-            .withNamedParameter("state", "SUCCESS_MARKER")
-            .withNamedParameter("bucketRegion", "SUCCESS_MARKER")
-            .withNamedParameter("data", "{}"));
-    return queryList;
-  }
-
-  protected static SQLQuery buildResetJobTableItemsForResumeStatement(String schema, Step step) {
-    return new SQLQuery("""
-        UPDATE ${schema}.${table}
-          SET state =
-            CASE
-              WHEN state = 'SUCCESS_MARKER_RUNNING' THEN 'SUCCESS_MARKER'
-              WHEN state = 'RUNNING' THEN 'SUBMITTED'
-              WHEN state = 'FAILED' THEN 'SUBMITTED'
-            END,
-              execution_count =
-                 CASE
-                   WHEN execution_count = 2 THEN 1
-                   ELSE execution_count
-                 END
-          WHERE state IN ('SUCCESS_MARKER_RUNNING', 'RUNNING', 'FAILED');
-        """)
-        .withVariable("schema", schema)
-        .withVariable("table", getTemporaryJobTableName(step.getId()));
   }
 
   protected static Map<String, Object> createQueryContext(String stepId, String schema, String table,
@@ -160,31 +64,48 @@ public class TransportTools {
     return queryContext;
   }
 
+  //TODO: Introduce proper logging methods at parent Step class level
+
+  @Deprecated
   protected static void infoLog(Phase phase, Step step, String... messages) {
     logger.info("{} [{}@{}] ON '{}' {}", step.getClass().getSimpleName(), step.getGlobalStepId(), phase.name(), getSpaceId(step),
         messages.length > 0 ? messages : "");
   }
 
-  protected static String getSpaceId(Step step) {
+  @Deprecated
+  private static String getSpaceId(Step step) {
     return step instanceof SpaceBasedStep<?> spaceStep ? spaceStep.getSpaceId() : null;
   }
 
+  @Deprecated
   protected static void errorLog(Phase phase, Step step, Exception e, String... message) {
     logger.error("{} [{}@{}] ON '{}' {}", step.getClass().getSimpleName(), step.getGlobalStepId(), phase.name(), getSpaceId(step),
         message, e);
   }
 
+  @Deprecated
   protected enum Phase {
+    @Deprecated
     GRAPH_TRANSFORMER,
+    @Deprecated
     JOB_EXECUTOR,
+    @Deprecated
     STEP_EXECUTE,
+    @Deprecated
     STEP_RESUME,
+    @Deprecated
     STEP_CANCEL,
+    @Deprecated
     STEP_ON_STATE_CHECK,
+    @Deprecated
     STEP_ON_ASYNC_FAILURE,
+    @Deprecated
     STEP_ON_ASYNC_UPDATE,
+    @Deprecated
     STEP_ON_ASYNC_SUCCESS,
+    @Deprecated
     JOB_DELETE,
+    @Deprecated
     JOB_VALIDATE
   }
 }
