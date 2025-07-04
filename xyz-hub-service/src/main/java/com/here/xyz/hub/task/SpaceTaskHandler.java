0615f5496714c6a3fbfa5c62d21070a88dbf8319
Benjamin Rögner <benjamin.roegner@here.com>
2025-07-04 13:42:42 +0200

    DS-1413: Add some debug outputs for space deletion
    Signed-off-by: Benjamin Rögner <benjamin.roegner@here.com>
    (cherry picked from commit 3d84a0e51a3442624ba4d6a4e9e5c34fe6700e04)
    Signed-off-by: Benjamin Rögner <benjamin.roegner@here.com>

--- xyz-hub-service/src/main/java/com/here/xyz/hub/task/SpaceTaskHandler.java
+++ xyz-hub-service/src/main/java/com/here/xyz/hub/task/SpaceTaskHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017-2023 HERE Europe B.V.
+ * Copyright (C) 2017-2025 HERE Europe B.V.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -397,7 +397,8 @@ public class SpaceTaskHandler {
       return;
     }
 
-    if (entry.input != null && entry.result == null)
+    if (entry.input != null && entry.result == null) {
+      logger.warn("DEBUG: Deleting space: {}", entry.head.getId());
       Service.spaceConfigClient
           .delete(task.getMarker(), entry.head.getId())
           .onFailure(callback::exception)
@@ -405,7 +406,9 @@ public class SpaceTaskHandler {
             task.responseSpaces = Collections.singletonList(task.modifyOp.entries.get(0).head);
             callback.call(task);
           });
-    else
+    }
+    else {
+      logger.warn("DEBUG: Storing space: {}", entry.result.getId());
       Service.spaceConfigClient
           .store(task.getMarker(), entry.result)
           .onFailure(callback::exception)
@@ -413,6 +416,7 @@ public class SpaceTaskHandler {
             task.responseSpaces = Collections.singletonList(entry.result);
             callback.call(task);
           });
+    }
   }
 
   private static Space getSpaceTemplate(String owner, String cid) {
