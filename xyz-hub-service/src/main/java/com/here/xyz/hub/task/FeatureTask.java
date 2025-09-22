04793d374f586fc43fd79fe96a462e6c25e52ff8
Ansari, Mujammil <mujammil.ansari@here.com>
2025-09-22 22:06:02 +0530

    Add BWC context support for branching
    Signed-off-by: Ansari, Mujammil <mujammil.ansari@here.com>

--- xyz-hub-service/src/main/java/com/here/xyz/hub/task/FeatureTask.java
+++ xyz-hub-service/src/main/java/com/here/xyz/hub/task/FeatureTask.java
@@ -19,7 +19,10 @@
 
 package com.here.xyz.hub.task;
 
+import static com.here.xyz.events.ContextAwareEvent.SpaceContext.EXTENSION;
+import static com.here.xyz.events.ContextAwareEvent.SpaceContext.SUPER;
 import static com.here.xyz.models.hub.Branch.MAIN_BRANCH;
+import static com.here.xyz.models.hub.Space.TABLE_NAME;
 import static io.netty.handler.codec.http.HttpResponseStatus.BAD_GATEWAY;
 import static io.netty.handler.codec.http.HttpResponseStatus.BAD_REQUEST;
 import static io.netty.handler.codec.http.HttpResponseStatus.INTERNAL_SERVER_ERROR;
@@ -65,6 +68,8 @@ import com.here.xyz.models.hub.FeatureModificationList.ConflictResolution;
 import com.here.xyz.models.hub.FeatureModificationList.IfExists;
 import com.here.xyz.models.hub.FeatureModificationList.IfNotExists;
 import com.here.xyz.models.hub.Ref;
+import com.here.xyz.psql.query.XyzEventBasedQueryRunner;
+import com.here.xyz.psql.query.branching.BranchManager;
 import com.here.xyz.responses.XyzResponse;
 import com.here.xyz.util.geo.GeometryValidator;
 import com.here.xyz.util.service.HttpException;
@@ -176,10 +181,21 @@ public abstract class FeatureTask<T extends Event<?>, X extends FeatureTask<T, ?
       }
     }
 
-    if (referencedBranch != null)
-      event
-          .withNodeId(referencedBranch.getNodeId())
-          .withBranchPath(getBranchPath(space, referencedBranch));
+    if (referencedBranch != null) {
+      LinkedList<Ref> branchPath = (LinkedList<Ref>) getBranchPath(space, referencedBranch);
+      event.withNodeId(referencedBranch.getNodeId())
+              .withBranchPath(branchPath);
+
+      //TODO: Remove this hack when SpaceContext support is removed from branching
+      if (event.getContext() == SUPER)  {
+        Ref lastRef = branchPath.removeLast();
+        event.setNodeId(BranchManager.getNodeId(lastRef));
+      } else if (event.getContext() == EXTENSION) {
+        event.setConnectorParams(Map.copyOf(space.getResolvedStorageConnector().params));
+        event.getParams().put(TABLE_NAME, XyzEventBasedQueryRunner.readBranchTableFromEvent(event));
+        event.setNodeId(0);
+      }
+    }
   }
 
   protected static Branch getReferencedBranch(Space space, Ref ref) throws HttpException {
