5876c18ae0e2973773e91f54905d51539d43b39c
Rohit Lakde <rohit.lakde@here.com>
2025-10-08 11:57:38 -0500

    DefaultStorageHandler Changes.

--- here-naksha-lib-handlers/src/main/java/com/here/naksha/lib/handlers/DefaultStorageHandler.java
+++ here-naksha-lib-handlers/src/main/java/com/here/naksha/lib/handlers/DefaultStorageHandler.java
@@ -295,6 +295,7 @@ public class DefaultStorageHandler extends AbstractEventHandler {
       return singleWrite(ctx, storageImpl, wr);
   }
 
+
   private @NotNull Result singleWrite(
       @NotNull NakshaContext ctx, @NotNull IStorage storageImpl, @NotNull WriteRequest<?, ?, ?> wr) {
     try (final IWriteSession writer = storageImpl.newWriteSession(ctx, true)) {
