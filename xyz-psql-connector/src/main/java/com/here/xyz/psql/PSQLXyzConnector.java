cadd56ef6bf0238fe31f57f529416dc02c296569
qGYdXbY2 <47661341+qGYdXbY2@users.noreply.github.com>
2025-10-24 17:44:49 +0200

    DS-1675: return 404 when iterate Changesets on space without history
    Signed-off-by: qGYdXbY2 <47661341+qGYdXbY2@users.noreply.github.com>

--- xyz-psql-connector/src/main/java/com/here/xyz/psql/PSQLXyzConnector.java
+++ xyz-psql-connector/src/main/java/com/here/xyz/psql/PSQLXyzConnector.java
@@ -201,8 +201,14 @@ public class PSQLXyzConnector extends DatabaseHandler {
     return write(new DeleteChangesets(event));
   }
 
+  private void validateHistoryEnabled(Event<?> event ) throws Exception
+  { if( event instanceof IterateChangesetsEvent e && e.getVersionsToKeep() <= 1 )
+     throw new ErrorResponseException(ILLEGAL_ARGUMENT,"Changesets needs history enabled. VersionsToKeep should be greater 1");
+  }
+
   @Override
   protected ChangesetCollection processIterateChangesetsEvent(IterateChangesetsEvent event) throws Exception {
+    validateHistoryEnabled( event );
     return run(new IterateChangesets(event));
   }
 
@@ -315,7 +321,7 @@ public class PSQLXyzConnector extends DatabaseHandler {
       }
 
       case "42P01":
-        
+
         throw new ErrorResponseException(NOT_FOUND, "Space not found in backend" ); //"Table not found in database"
 
       case
