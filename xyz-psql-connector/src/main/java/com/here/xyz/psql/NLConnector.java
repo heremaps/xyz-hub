b27277d8238af175e61d5879518a4884b7cadba6
VladyslavHnes <vladyslav.hnes@gmail.com>
2025-11-21 13:59:33 +0200

    DS-1782 java 11 changes
    Signed-off-by: VladyslavHnes <vladyslav.hnes@gmail.com>

--- xyz-psql-connector/src/main/java/com/here/xyz/psql/NLConnector.java
+++ xyz-psql-connector/src/main/java/com/here/xyz/psql/NLConnector.java
@@ -516,66 +516,63 @@ public class NLConnector extends PSQLXyzConnector {
     String baseTable = tables.size() == 2 ? tables.get(1) : null;
 
     if(baseTable == null) {
-      return """
-               WITH params AS (
-                   SELECT '$refQuad$'::text AS parent, $quadKeyLevel$ AS relative_level
-               )
-               SELECT LEFT(searchable->>'refQuad', LENGTH(parent) + relative_level) AS child_quad,
-                      COUNT(*) AS cnt
-               FROM "$schema$"."$table$", params
-               	  WHERE searchable->>'refQuad' LIKE parent || '%'
-               GROUP BY child_quad;
-              """
+      return ("WITH params AS (\n"
+              + "    SELECT '$refQuad$'::text AS parent, $quadKeyLevel$ AS relative_level\n"
+              + ")\n"
+              + "SELECT LEFT(searchable->>'refQuad', LENGTH(parent) + relative_level) AS child_quad,\n"
+              + "       COUNT(*) AS cnt\n"
+              + "FROM \"$schema$\".\"$table$\", params\n"
+              + "	  WHERE searchable->>'refQuad' LIKE parent || '%'\n"
+              + "GROUP BY child_quad;\n")
               .replace("$refQuad$", refQuad)
               .replace("$quadKeyLevel$", Integer.toString(quadKeyLevel))
               .replace("$schema$", schema)
               .replace("$table$", extensionTable + "_head");
     }
-    return """
-          WITH params AS (
-              SELECT '$refQuad$'::text AS parent, $quadKeyLevel$ AS relative_level
-          ),
-          combined AS (
-            SELECT *
-            FROM (
-                SELECT
-                    id,
-                    searchable->>'refQuad' AS refquad,
-                    operation,
-                    next_version
-                FROM "$schema$"."$extensionTable$"
-                WHERE operation NOT IN ('D', 'H', 'J')
-            )
-            UNION ALL
-            (
-                SELECT *
-                FROM (
-                    SELECT
-                        id,
-                        searchable->>'refQuad' AS refquad,
-                        operation,
-                        next_version
-                    FROM "$schema$"."$baseTable$"
-                ) base
-                WHERE NOT EXISTS (
-                    SELECT 1
-                    FROM "$schema$"."$extensionTable$"
-                    WHERE id = base.id
-                      AND next_version = 9223372036854775807::BIGINT
-                      AND operation != 'D'
-                )
-              )
-            ),
-            filtered AS (
-                SELECT c.*
-                  FROM combined c
-                 WHERE c.refquad LIKE (SELECT parent FROM params) || '%'
-            )
-            SELECT LEFT(f.refquad, LENGTH(p.parent) + p.relative_level) AS child_quad,
-                   COUNT(f.refquad) AS cnt
-              FROM filtered f, params p
-             GROUP BY child_quad;
-          """.replace("$refQuad$", refQuad)
+    return ("WITH params AS (\n"
+          + "    SELECT '$refQuad$'::text AS parent, $quadKeyLevel$ AS relative_level\n"
+          + "),\n"
+          + "combined AS (\n"
+          + "  SELECT *\n"
+          + "  FROM (\n"
+          + "      SELECT\n"
+          + "          id,\n"
+          + "          searchable->>'refQuad' AS refquad,\n"
+          + "          operation,\n"
+          + "          next_version\n"
+          + "      FROM \"$schema$\".\"$extensionTable$\"\n"
+          + "      WHERE operation NOT IN ('D', 'H', 'J')\n"
+          + "  )\n"
+          + "  UNION ALL\n"
+          + "  (\n"
+          + "      SELECT *\n"
+          + "      FROM (\n"
+          + "          SELECT\n"
+          + "              id,\n"
+          + "              searchable->>'refQuad' AS refquad,\n"
+          + "              operation,\n"
+          + "              next_version\n"
+          + "          FROM \"$schema$\".\"$baseTable$\"\n"
+          + "      ) base\n"
+          + "      WHERE NOT EXISTS (\n"
+          + "          SELECT 1\n"
+          + "          FROM \"$schema$\".\"$extensionTable$\"\n"
+          + "          WHERE id = base.id\n"
+          + "            AND next_version = 9223372036854775807::BIGINT\n"
+          + "            AND operation != 'D'\n"
+          + "      )\n"
+          + "    )\n"
+          + "  ),\n"
+          + "  filtered AS (\n"
+          + "      SELECT c.*\n"
+          + "        FROM combined c\n"
+          + "       WHERE c.refquad LIKE (SELECT parent FROM params) || '%'\n"
+          + "  )\n"
+          + "  SELECT LEFT(f.refquad, LENGTH(p.parent) + p.relative_level) AS child_quad,\n"
+          + "         COUNT(f.refquad) AS cnt\n"
+          + "    FROM filtered f, params p\n"
+          + "   GROUP BY child_quad;\n")
+            .replace("$refQuad$", refQuad)
             .replace("$quadKeyLevel$", Integer.toString(quadKeyLevel))
             .replace("$schema$", schema)
             .replace("$baseTable$", baseTable + "_head")
@@ -597,26 +594,22 @@ public class NLConnector extends PSQLXyzConnector {
     }
 
     if(baseTable == null)
-      return """
-               SELECT operation, COUNT(*) AS cnt
-                FROM "$schema$"."$table$"
-               WHERE operation IN($operations$)
-               GROUP BY operation;
-              """
+      return ("SELECT operation, COUNT(*) AS cnt\n"
+               + " FROM \"$schema$\".\"$table$\"\n"
+               + "WHERE operation IN($operations$)\n"
+               + "GROUP BY operation;\n")
              .replace("$operations$", opsString)
              .replace("$schema$", schema)
              .replace("$table$", extensionTable + "_head");
-    return """
-            SELECT operation, COUNT(*) AS cnt
-            FROM (
-              SELECT operation FROM "$schema$"."$baseTable$"
-                WHERE operation IN($operations$)
-              UNION ALL
-              SELECT operation FROM "$schema$"."$extensionTable$"
-                WHERE operation IN($operations$)
-            ) AS combined
-            GROUP BY operation
-            """
+    return ("SELECT operation, COUNT(*) AS cnt\n"
+            + "FROM (\n"
+            + "  SELECT operation FROM \"$schema$\".\"$baseTable$\"\n"
+            + "    WHERE operation IN($operations$)\n"
+            + "  UNION ALL\n"
+            + "  SELECT operation FROM \"$schema$\".\"$extensionTable$\"\n"
+            + "    WHERE operation IN($operations$)\n"
+            + ") AS combined\n"
+            + "GROUP BY operation\n")
             .replace("$operations$", opsString)
             .replace("$schema$", schema)
             .replace("$baseTable$", baseTable + "_head")
@@ -663,15 +656,13 @@ public class NLConnector extends PSQLXyzConnector {
       finalQuery = inner1;
     }else {
       String inner2 = "SELECT * FROM \"" + schema + "\".\"" + baseTable + "_head\" " + filter;
-      String whereNotExistsCondition = """
-            WHERE NOT EXISTS (
-                  SELECT 1
-                  	FROM "$schema$"."$table$"
-                  WHERE id = a.id
-                    AND next_version = 9223372036854775807::BIGINT
-                    AND operation != 'D'
-                )
-            """
+      String whereNotExistsCondition = ("WHERE NOT EXISTS (\n"
+            + "      SELECT 1\n"
+            + "      	FROM \"$schema$\".\"$table$\"\n"
+            + "      WHERE id = a.id\n"
+            + "        AND next_version = 9223372036854775807::BIGINT\n"
+            + "        AND operation != 'D'\n"
+            + "    )\n")
               .replace("$schema$", schema)
               .replace("$table$", extensionTable);
       // UNION ALL combined set
@@ -818,25 +809,25 @@ public class NLConnector extends PSQLXyzConnector {
           List<ModificationFailure> fails
   ) throws SQLException, JsonProcessingException {
 
-    String mergeSql = """
-        MERGE INTO %s.%s AS t
-        USING (
-          VALUES (?, ?, ?, ?, ?, ?, ?)
-        ) AS s(id, geo, operation, author, version, jsondata, searchable)
-        --ON (t.id = s.id AND t.next_version = 9223372036854775807 AND t.global_version = s.global_version)
-        ON (t.id = s.id AND t.version = s.version AND t.next_version = 9223372036854775807)
-        WHEN MATCHED THEN
-          UPDATE SET
-            jsondata  = s.jsondata,
-            geo       = s.geo,
-            version   = s.version,
-            author    = s.author,
-            operation = 'U',
-            searchable = s.searchable
-        WHEN NOT MATCHED THEN
-          INSERT (id, geo, operation, author, version, jsondata, searchable)
-          VALUES (s.id, s.geo, s.operation, s.author, s.version, s.jsondata, s.searchable);
-        """.formatted(schema, table);
+    String mergeSql = String.format(
+        "MERGE INTO %s.%s AS t\n"
+        + "USING (\n"
+        + "  VALUES (?, ?, ?, ?, ?, ?, ?)\n"
+        + ") AS s(id, geo, operation, author, version, jsondata, searchable)\n"
+        + "--ON (t.id = s.id AND t.next_version = 9223372036854775807 AND t.global_version = s.global_version)\n"
+        + "ON (t.id = s.id AND t.version = s.version AND t.next_version = 9223372036854775807)\n"
+        + "WHEN MATCHED THEN\n"
+        + "  UPDATE SET\n"
+        + "    jsondata  = s.jsondata,\n"
+        + "    geo       = s.geo,\n"
+        + "    version   = s.version,\n"
+        + "    author    = s.author,\n"
+        + "    operation = 'U',\n"
+        + "    searchable = s.searchable\n"
+        + "WHEN NOT MATCHED THEN\n"
+        + "  INSERT (id, geo, operation, author, version, jsondata, searchable)\n"
+        + "  VALUES (s.id, s.geo, s.operation, s.author, s.version, s.jsondata, s.searchable);\n",
+        schema, table);
 
     try (Connection connection = dataSourceProvider.getWriter().getConnection()) {
       connection.setAutoCommit(false);
