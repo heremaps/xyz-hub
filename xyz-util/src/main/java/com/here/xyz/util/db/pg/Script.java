aa6dc2976a3f7ca966005f8a6f4657e785bc635b
qGYdXbY2 <47661341+qGYdXbY2@users.noreply.github.com>
2025-12-04 13:55:00 +0100

    DS-1801: fix js script installation
    Signed-off-by: qGYdXbY2 <47661341+qGYdXbY2@users.noreply.github.com>

--- xyz-util/src/main/java/com/here/xyz/util/db/pg/Script.java
+++ xyz-util/src/main/java/com/here/xyz/util/db/pg/Script.java
@@ -311,7 +311,9 @@ public class Script {
   private static List<String> scanResourceFolder(ScriptResourcePath scriptResourcePath, String fileSuffix) throws IOException {
     String resourceFolder = scriptResourcePath.path();
     //TODO: Remove this workaround once the actual implementation of this method supports scanning folders inside a JAR
-    if ("/sql".equals(resourceFolder) || "/jobs".equals(resourceFolder))
+    if (   "/sql".equals(resourceFolder)
+        || "/sql/lib-js".equals(resourceFolder)
+        || "/jobs".equals(resourceFolder))
       return ensureInitScriptIsFirst(scanResourceFolderWA(resourceFolder, fileSuffix), scriptResourcePath.initScript());
 
     final InputStream folderResource = Script.class.getResourceAsStream(resourceFolder);
