629900dccdb82c1503179f2a51ed6f53083ed111
Emil Obrusnik <eobrusnik@users.noreply.github.com>
2026-01-20 15:06:30 +0100

    DS-1946 add DataReference model
    Signed-off-by: Emil Obrusnik <eobrusnik@users.noreply.github.com>

---
+++ xyz-models/src/main/java/com/here/xyz/models/hub/DataReference.java
@@ -0,0 +1,208 @@
+package com.here.xyz.models.hub;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.here.xyz.XyzSerializable;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import java.util.UUID;
+
+// TODO: This really begs for Lombok. And to be an immutable class, with a builder.
+@JsonIgnoreProperties(ignoreUnknown = true)
+public final class DataReference implements XyzSerializable {
+
+  private UUID id;
+
+  private String entityId;
+
+  private boolean isPatch;
+
+  private Integer startVersion;
+
+  private Integer endVersion;
+
+  private String objectType;
+
+  private String contentType;
+
+  private String location;
+
+  private String sourceSystem;
+
+  private String targetSystem;
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public DataReference withId(UUID id) {
+    setId(id);
+    return this;
+  }
+
+  public String getEntityId() {
+    return entityId;
+  }
+
+  public void setEntityId(String entityId) {
+    this.entityId = entityId;
+  }
+
+  public DataReference withEntityId(String entityId) {
+    setEntityId(entityId);
+    return this;
+  }
+
+  @JsonProperty("isPatch")
+  public boolean isPatch() {
+    return isPatch;
+  }
+
+  public void setPatch(boolean patch) {
+    isPatch = patch;
+  }
+
+  public DataReference withPatch(boolean patch) {
+    setPatch(patch);
+    return this;
+  }
+
+  public Integer getStartVersion() {
+    return startVersion;
+  }
+
+  public void setStartVersion(Integer startVersion) {
+    this.startVersion = startVersion;
+  }
+
+  public DataReference withStartVersion(Integer startVersion) {
+    setStartVersion(startVersion);
+    return this;
+  }
+
+  public Integer getEndVersion() {
+    return endVersion;
+  }
+
+  public void setEndVersion(Integer endVersion) {
+    this.endVersion = endVersion;
+  }
+
+  public DataReference withEndVersion(Integer endVersion) {
+    setEndVersion(endVersion);
+    return this;
+  }
+
+  public String getObjectType() {
+    return objectType;
+  }
+
+  public void setObjectType(String objectType) {
+    this.objectType = objectType;
+  }
+
+  public DataReference withObjectType(String objectType) {
+    setObjectType(objectType);
+    return this;
+  }
+
+  public String getContentType() {
+    return contentType;
+  }
+
+  public void setContentType(String contentType) {
+    this.contentType = contentType;
+  }
+
+  public DataReference withContentType(String contentType) {
+    setContentType(contentType);
+    return this;
+  }
+
+  public String getLocation() {
+    return location;
+  }
+
+  public void setLocation(String location) {
+    this.location = location;
+  }
+
+  public DataReference withLocation(String location) {
+    setLocation(location);
+    return this;
+  }
+
+  public String getSourceSystem() {
+    return sourceSystem;
+  }
+
+  public void setSourceSystem(String sourceSystem) {
+    this.sourceSystem = sourceSystem;
+  }
+
+  public DataReference withSourceSystem(String sourceSystem) {
+    setSourceSystem(sourceSystem);
+    return this;
+  }
+
+  public String getTargetSystem() {
+    return targetSystem;
+  }
+
+  public void setTargetSystem(String targetSystem) {
+    this.targetSystem = targetSystem;
+  }
+
+  public DataReference withTargetSystem(String targetSystem) {
+    setTargetSystem(targetSystem);
+    return this;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    DataReference reference = (DataReference) o;
+
+    return new EqualsBuilder()
+      .append(isPatch(), reference.isPatch())
+      .append(getId(), reference.getId())
+      .append(getEntityId(), reference.getEntityId())
+      .append(getStartVersion(), reference.getStartVersion())
+      .append(getEndVersion(), reference.getEndVersion())
+      .append(getObjectType(), reference.getObjectType())
+      .append(getContentType(), reference.getContentType())
+      .append(getLocation(), reference.getLocation())
+      .append(getSourceSystem(), reference.getSourceSystem())
+      .append(getTargetSystem(), reference.getTargetSystem())
+      .isEquals();
+  }
+
+  @Override
+  public int hashCode() {
+    return new HashCodeBuilder(17, 37)
+      .append(getId())
+      .append(getEntityId())
+      .append(isPatch())
+      .append(getStartVersion())
+      .append(getEndVersion())
+      .append(getObjectType())
+      .append(getContentType())
+      .append(getLocation())
+      .append(getSourceSystem())
+      .append(getTargetSystem())
+      .toHashCode();
+  }
+
+}
