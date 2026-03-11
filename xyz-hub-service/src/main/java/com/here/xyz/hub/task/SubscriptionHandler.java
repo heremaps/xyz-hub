59acbb0ace435ce6b032acd62542f840c4fe5899
qGYdXbY2 <47661341+qGYdXbY2@users.noreply.github.com>
2026-03-11 14:14:07 +0100

    adapt resolveDependenciesForCreation
    Signed-off-by: qGYdXbY2 <47661341+qGYdXbY2@users.noreply.github.com>

--- xyz-hub-service/src/main/java/com/here/xyz/hub/task/SubscriptionHandler.java
+++ xyz-hub-service/src/main/java/com/here/xyz/hub/task/SubscriptionHandler.java
@@ -140,7 +140,7 @@ public class SubscriptionHandler {
         });
   }
 
-  private static int v2kForSubscribedNonHistoryLayers = 99991;
+  public static int v2kForSubscribedNonHistoryLayers = 99991;
 
   private static Future<Void> increaseVersionsToKeepIfNecessary(Marker marker, Space space) {
     return space != null && space.getVersionsToKeep() == 1 ?
