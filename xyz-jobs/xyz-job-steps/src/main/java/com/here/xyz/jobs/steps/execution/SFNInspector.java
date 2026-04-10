0280dfc7f615cc1d283a5c10430e85fdd261ed48
mchrza <maximilian.chrzan@here.com>
2026-04-10 09:49:08 +0200

    MMSUP-2328 Add SFNInspector tests.
    Signed-off-by: mchrza <maximilian.chrzan@here.com>

--- xyz-jobs/xyz-job-steps/src/main/java/com/here/xyz/jobs/steps/execution/SFNInspector.java
+++ xyz-jobs/xyz-job-steps/src/main/java/com/here/xyz/jobs/steps/execution/SFNInspector.java
@@ -1,6 +1,23 @@
+/*
+ * Copyright (C) 2017-2026 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
 package com.here.xyz.jobs.steps.execution;
 
-import com.here.xyz.jobs.steps.Config;
 import io.vertx.core.Future;
 import software.amazon.awssdk.services.sfn.model.DescribeExecutionRequest;
 import software.amazon.awssdk.services.sfn.model.DescribeExecutionResponse;
@@ -11,7 +28,6 @@ import software.amazon.awssdk.services.sfn.model.StateEnteredEventDetails;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 import static com.here.xyz.jobs.util.AwsClientFactory.asyncSfnClient;
 import static com.here.xyz.jobs.util.AwsClientFactory.sfnClient;
