cf3e0277ef8c0ce0a2fc12d4a01164b9b198c4a1
jaksa <44367804+MateuszJaksa@users.noreply.github.com>
2026-06-02 17:30:50 +0200

    fixing logger issue in spark v2
    Signed-off-by: jaksa <44367804+MateuszJaksa@users.noreply.github.com>

--- xyz-util/src/main/java/com/here/xyz/util/datasets/filters/JsonPathFilterUtils.java
+++ xyz-util/src/main/java/com/here/xyz/util/datasets/filters/JsonPathFilterUtils.java
@@ -19,7 +19,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.here.xyz.models.geojson.implementation.Feature;
-import com.here.xyz.models.geojson.implementation.Properties;
 import com.jayway.jsonpath.Configuration;
 import com.jayway.jsonpath.JsonPath;
 import com.jayway.jsonpath.PathNotFoundException;
