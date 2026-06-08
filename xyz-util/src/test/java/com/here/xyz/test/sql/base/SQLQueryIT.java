d90715b3c0e78303ca61fdb0e8e6616e7f9fffe0
mchrza <maximilian.chrzan@here.com>
2026-06-08 16:34:55 +0200

    DS-2171: Enhance TaskedSpaceBasedStep - task query recovery for serverside killed queries.
    Change job_data table layout - add new columns: unknown_query_state_cnt,retry_attempts,started_at,updated_at.
    TaskProgress: extend Model with startedNotFinalizedTaskIds - to be able to know the exact running taksIds.
    SQLQuery: add areRunning() method to check multiple queries if they are running.
    TaskedSpaceBasedStep:
    * update all job_data write queries to maintain new timestamps
    * add a new taskId label to the running taskQueries
    * improve getExecutionState() handling. Use expected taskIds to check if the related Queries are running. Add a retry mechanism for queries which are disappeared (eg. server side termination)
    
    Signed-off-by: mchrza <maximilian.chrzan@here.com>

--- xyz-util/src/test/java/com/here/xyz/test/sql/base/SQLQueryIT.java
+++ xyz-util/src/test/java/com/here/xyz/test/sql/base/SQLQueryIT.java
@@ -65,6 +65,70 @@ public class SQLQueryIT extends SQLITBase {
   }
 
   @Test
+  public void areRunningCheckMultipleValuesForSameLabel() throws Exception {
+    try (DataSourceProvider dsp = getDataSourceProvider()) {
+      SQLQuery longRunningQuery1 = new SQLQuery("SELECT pg_sleep(500)")
+          .withLabel("taskId", "1");
+      SQLQuery longRunningQuery2 = new SQLQuery("SELECT pg_sleep(500)")
+          .withLabel("taskId", "2");
+
+      new Thread(() -> {
+        try {
+          longRunningQuery1.run(dsp);
+        }
+        catch (SQLException e) {
+          //Ignore
+        }
+      }).start();
+
+      new Thread(() -> {
+        try {
+          longRunningQuery2.run(dsp);
+        }
+        catch (SQLException e) {
+          //Ignore
+        }
+      }).start();
+
+      //Wait until both queries are visible in pg_stat_activity.
+      long deadline = System.currentTimeMillis() + 5_000;
+      while (System.currentTimeMillis() < deadline && SQLQuery.areRunning(dsp, false, "taskId", Set.of("1", "2")).size() < 2)
+        Thread.sleep(100);
+
+      //Check that both taskIds are currently running
+      assertEquals(Set.of(
+              "1",
+              "2"
+      ), SQLQuery.areRunning(dsp, false, "taskId", Set.of(
+              "1",
+              "2"
+      )));
+
+      assertEquals(Set.of(
+              "1"
+      ), SQLQuery.areRunning(dsp, false, "taskId", Set.of(
+              "1"
+      )));
+
+      assertEquals(Set.of(
+              "2"
+      ), SQLQuery.areRunning(dsp, false, "taskId", Set.of(
+              "2"
+      )));
+
+      //Kill the long-running queries
+      longRunningQuery1.kill();
+      longRunningQuery2.kill();
+
+      //Check that the original queries are not running anymore
+      assertFalse(SQLQuery.isRunning(dsp, false, longRunningQuery1.getQueryId()));
+      assertFalse(SQLQuery.isRunning(dsp, false, longRunningQuery2.getQueryId()));
+      assertEquals(Set.of(), SQLQuery.areRunning(dsp, false, "taskId", Set.of(
+             "1","2")));
+    }
+  }
+
+  @Test
   public void runAsyncQuery() throws Exception {
     SQLQuery longRunningAsyncQuery = new SQLQuery("SELECT pg_sleep(10)").withAsync(true);
 
